49754bbc0a7927641f2acb5220472e8a38286ff9
Summer G Stapleton <sgstapleton@usgs.gov>
2018-11-29 16:40:33 -0700

    Very simple implementation (with example code) of a state-from-isd method.

---
+++ src/eal.cpp
@@ -0,0 +1,19 @@
+
+#include <json.hpp>
+
+using json = nlohmann::json;
+
+
+std::string eal::constructStateFromIsd(const std::string positionRotationData) const
+{
+   // Parse the position and rotation data from isd
+   json isd = json::parse(positionRotationData);
+   json state;
+
+   state["m_w"] = isd.at("w");
+   state["m_x"] = isd.at("x");
+   state["m_y"] = isd.at("y");
+   state["m_z"] = isd.at("z");
+
+   return state.dump();
+}
