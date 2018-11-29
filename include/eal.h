ce25246dff713fa1cf2783ce338b03dc913a4df0
Adam Paquette <acp263@nau.edu>
2018-11-29 16:42:57 -0700

    Added functions to h file and update library name in cmake files

--- include/eal.h
+++ include/eal.h
@@ -12,4 +12,10 @@ vector<double> getVelocity(vector<vector<double>> coords, vector<double> coeffs,
 vector<double> getPosition(vector<double> coeffs, char *interp, double time);
 vector<double> getVelocity(vector<double> coeffs, char *interp, double time, bool interpolation);
 
+vector<double> getRotation(char *from, char *to, vector<vector<double>> rotations, vector<double> times, char *interp, double time);
+vector<double> getAngularVelocity(char *from, char *to, vector<vector<double>> rotations, vector<double> times, char *interp, double time);
+
+vector<double> getRotation(char *from, char *to, vector<double> coefficients, char *interp, double time);
+vector<double> getAngularVelocity(char *from, char *to, vector<double> coefficients, char *interp, double time);
+
 #endif // EAL_H
