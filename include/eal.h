0558e5d437d9fc770407b6423c22abad387828eb
Jesse Mapel <jmapel@usgs.gov>
2018-11-30 12:16:22 -0700

    Fixed interpolation options

--- include/eal.h
+++ include/eal.h
@@ -8,17 +8,23 @@ using namespace std;
 
 namespace eal {
 
-  vector<double> getPosition(vector<vector<double>> coords, vector<double> times, string interp, double time);
-  vector<double> getVelocity(vector<vector<double>> coords, vector<double> times, string interp, double time, bool interpolation);
+  vector<double> getPosition(vector<vector<double>> coords, vector<double> times,
+                             string interp, double time);
+  vector<double> getVelocity(vector<vector<double>> coords, vector<double> times,
+                             string interp, double time, bool interpolation);
 
   vector<double> getPosition(vector<double> coeffs, string interp, double time);
-  vector<double> getVelocity(vector<double> coeffs, string interp, double time, bool interpolation);
+  vector<double> getVelocity(vector<double> coeffs, string interp, double time);
 
-  vector<double> getRotation(string from, string to, vector<vector<double>> rotations, vector<double> times, string interp, double time);
-  vector<double> getAngularVelocity(string from, string to, vector<vector<double>> rotations, vector<double> times, string interp, double time);
+  vector<double> getRotation(string from, string to, vector<vector<double>> rotations,
+                             vector<double> times, string interp, double time);
+  vector<double> getAngularVelocity(string from, string to, vector<vector<double>> rotations,
+                                    vector<double> times, string interp, double time, bool interpolation);
 
-  vector<double> getRotation(string from, string to, vector<double> coefficients, string interp, double time);
-  vector<double> getAngularVelocity(string from, string to, vector<double> coefficients, string interp, double time);
+  vector<double> getRotation(string from, string to, vector<double> coefficients,
+                             string interp, double time);
+  vector<double> getAngularVelocity(string from, string to, vector<double> coefficients,
+                                    string interp, double time);
 
 }
 
