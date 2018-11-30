3e8f2fc4f97072da6fb4112118b0c34e3b7b4dce
Jesse Mapel <jmapel@usgs.gov>
2018-11-30 12:16:22 -0700

    Fixed interpolation options

--- src/eal.cpp
+++ src/eal.cpp
@@ -5,12 +5,14 @@ using namespace eal;
 // Positional Functions
 
 // Position Data Functions
-vector<double> getPosition(vector<vector<double>> coords, vector<double> times, string interp, double time) {
+vector<double> getPosition(vector<vector<double>> coords, vector<double> times,
+                           string interp, double time) {
   vector<double> coordinate = {0.0, 0.0, 0.0};
   return coordinate;
 }
 
-vector<double> getVelocity(vector<vector<double>> coords, vector<double> times, string interp, double time, bool interpolation) {
+vector<double> getVelocity(vector<vector<double>> coords, vector<double> times,
+                           string interp, double time, bool interpolation) {
   vector<double> coordinate = {0.0, 0.0, 0.0};
   return coordinate;
 }
@@ -21,7 +23,8 @@ vector<double> getPosition(vector<double> coeffs, string interp, double time) {
   return coordinate;
 }
 
-vector<double> getVelocity(vector<double> coeffs, string interp, double time, bool interpolation) {
+vector<double> getVelocity(vector<double> coeffs, string interp, double time,
+                           bool interpolation) {
   vector<double> coordinate = {0.0, 0.0, 0.0};
   return coordinate;
 }
@@ -29,23 +32,27 @@ vector<double> getVelocity(vector<double> coeffs, string interp, double time, bo
 // Rotation Data Functions
 
 // Rotation Data Functions
-vector<double> getRotation(string from, string to, vector<vector<double>> rotations, vector<double> times, string interp, double time) {
+vector<double> getRotation(string from, string to, vector<vector<double>> rotations,
+                           vector<double> times, string interp, double time) {
   vector<double> coordinate = {0.0, 0.0, 0.0};
   return coordinate;
 }
 
-vector<double> getAngularVelocity(string from, string to, vector<vector<double>> rotations, vector<double> times, string interp, double time) {
+vector<double> getAngularVelocity(string from, string to, vector<vector<double>> rotations,
+                                  vector<double> times, string interp, double time, bool interpolation) {
   vector<double> coordinate = {0.0, 0.0, 0.0};
   return coordinate;
 }
 
 // Rotation Function Functions
-vector<double> getRotation(string from, string to, vector<double> coefficients, string interp, double time) {
+vector<double> getRotation(string from, string to, vector<double> coefficients,
+                           string interp, double time) {
   vector<double> coordinate = {0.0, 0.0, 0.0};
   return coordinate;
 }
 
-vector<double> getAngularVelocity(string from, string to, vector<double> coefficients, string interp, double time) {
+vector<double> getAngularVelocity(string from, string to, vector<double> coefficients,
+                                  string interp, double time) {
   vector<double> coordinate = {0.0, 0.0, 0.0};
   return coordinate;
 }
