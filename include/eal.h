b8b01e697ed1e4a7755032d5346e2750ea371aae
Kelvin Rodriguez <kelvinrr@icloud.com>
2018-12-04 09:02:03 -0700

    implemented getVelocity

--- include/eal.h
+++ include/eal.h
@@ -5,21 +5,25 @@
 #include <string>
 #include <vector>
 
+#include <gsl/gsl_interp.h>
+
 namespace eal {
 
-  enum interpolation {
-    linear,
-    spline
+  enum derivation {
+    none,
+    first,
+    second
   };
 
   nlohmann::json constructStateFromIsd(const std::string positionRotationData);
 
   std::vector<double> getPosition(std::vector<std::vector<double>> coords,
                                   std::vector<double> times,
-                                  interpolation interp, double time);
+                                  double time, const gsl_interp_type* interp);
+
   std::vector<double> getVelocity(std::vector<std::vector<double>> coords,
                                   std::vector<double> times,
-                                  interpolation interp, double time);
+                                  double time, const gsl_interp_type* interp);
 
   std::vector<double> getPosition(std::vector<double> coeffs, double time);
   std::vector<double> getVelocity(std::vector<double> coeffs, double time);
@@ -27,19 +31,20 @@ namespace eal {
   std::vector<double> getRotation(std::string from, std::string to,
                                   std::vector<std::vector<double>> rotations,
                                   std::vector<double> times,
-                                  interpolation interp, double time);
+                                  double time, const gsl_interp_type* interp);
   std::vector<double> getAngularVelocity(std::string from, std::string to,
                                          std::vector<std::vector<double>> rotations,
                                          std::vector<double> times,
-                                         interpolation interp, double time);
+                                         double time, const gsl_interp_type* interp);
 
   std::vector<double> getRotation(std::string from, std::string to,
                                   std::vector<double> coefficients, double time);
   std::vector<double> getAngularVelocity(std::string from, std::string to,
                                          std::vector<double> coefficients, double time);
 
-  double linearInterpolate(std::vector<double> points, std::vector<double> times, double time);
-  double splineInterpolate(std::vector<double> points, std::vector<double> times, double time);
+  double interpolate(std::vector<double> points, std::vector<double> times, double time, const gsl_interp_type* interp, derivation d);
+
+
 }
 
 #endif // EAL_H
--- src/eal.cpp
+++ src/eal.cpp
@@ -30,8 +30,8 @@ namespace eal {
   // Positional Functions
 
   // Position Data Functions
-  vector<double> getPosition(vector<vector<double>> coords, vector<double> times,
-                             interpolation interp, double time) {
+  vector<double> getPosition(vector<vector<double>> coords, vector<double> times, double time,
+                             const gsl_interp_type* interp) {
     // Check that all of the data sizes are okay
     // TODO is there a cleaner way to do this? We're going to have to do this a lot.
     if (coords.size() != 3) {
@@ -40,30 +40,29 @@ namespace eal {
 
     // GSL setup
     vector<double> coordinate = {0.0, 0.0, 0.0};
-    switch(interp) {
-      case linear:
-        coordinate = { linearInterpolate(coords[0], times, time),
-                       linearInterpolate(coords[1], times, time),
-                       linearInterpolate(coords[2], times, time) };
-        break;
 
-      case spline:
-      coordinate = { splineInterpolate(coords[0], times, time),
-                     splineInterpolate(coords[1], times, time),
-                     splineInterpolate(coords[2], times, time) };
-        break;
-
-      default:
-        throw invalid_argument("Invalid interpolation method.");
-        break;
-    }
+    coordinate = { interpolate(coords[0], times, time, interp, none),
+                   interpolate(coords[1], times, time, interp, none),
+                   interpolate(coords[2], times, time, interp, none) };
 
     return coordinate;
   }
 
   vector<double> getVelocity(vector<vector<double>> coords, vector<double> times,
-                             interpolation interp, double time) {
+                             double time, const gsl_interp_type* interp) {
+    // Check that all of the data sizes are okay
+    // TODO is there a cleaner way to do this? We're going to have to do this a lot.
+    if (coords.size() != 3) {
+     throw invalid_argument("Invalid input positions, expected three vectors.");
+    }
+
+    // GSL setup
     vector<double> coordinate = {0.0, 0.0, 0.0};
+
+    coordinate = { interpolate(coords[0], times, time, interp, first),
+                   interpolate(coords[1], times, time, interp, first),
+                   interpolate(coords[2], times, time, interp, first) };
+
     return coordinate;
   }
 
@@ -82,13 +81,13 @@ namespace eal {
 
   // Rotation Data Functions
   vector<double> getRotation(string from, string to, vector<vector<double>> rotations,
-                             vector<double> times, interpolation interp, double time) {
+                             vector<double> times, double time,  const gsl_interp_type* interp) {
     vector<double> coordinate = {0.0, 0.0, 0.0};
     return coordinate;
   }
 
   vector<double> getAngularVelocity(string from, string to, vector<vector<double>> rotations,
-                                    vector<double> times, interpolation interp, double time) {
+                                    vector<double> times, double time,  const gsl_interp_type* interp) {
     vector<double> coordinate = {0.0, 0.0, 0.0};
     return coordinate;
   }
@@ -106,59 +105,45 @@ namespace eal {
     return coordinate;
   }
 
-  // Interpolation helper functions
-  double linearInterpolate(vector<double> points, vector<double> times, double time) {
-    size_t numPoints = points.size();
-    if (numPoints < 2) {
-      throw invalid_argument("At least two points must be input to interpolate over.");
-    }
-    if (points.size() != times.size()) {
-      throw invalid_argument("Invalid interpolation data, must have the same number of points as times.");
-    }
-    if (time < times.front() || time > times.back()) {
-      throw invalid_argument("Invalid interpolation time, outside of input times.");
-    }
 
-    // GSL setup
-    gsl_interp *interpolator = gsl_interp_alloc(gsl_interp_linear, numPoints);
-    gsl_interp_init(interpolator, &times[0], &points[0], numPoints);
-    gsl_interp_accel *acc = gsl_interp_accel_alloc();
+ double interpolate(vector<double> points, vector<double> times, double time, const gsl_interp_type* interp, derivation d) {
+   size_t numPoints = points.size();
+   if (numPoints < 2) {
+     throw invalid_argument("At least two points must be input to interpolate over.");
+   }
+   if (points.size() != times.size()) {
+     throw invalid_argument("Invalid gsl_interp_type data, must have the same number of points as times.");
+   }
+   if (time < times.front() || time > times.back()) {
+     throw invalid_argument("Invalid gsl_interp_type time, outside of input times.");
+   }
 
-    // GSL evaluate
-    double result = gsl_interp_eval(interpolator, &times[0], &points[0], time, acc);
+   gsl_interp *interpolator = gsl_interp_alloc(interp, numPoints);
+   gsl_interp_init(interpolator, &times[0], &points[0], numPoints);
+   gsl_interp_accel *acc = gsl_interp_accel_alloc();
 
-    // GSL clean up
-    gsl_interp_free(interpolator);
-    gsl_interp_accel_free(acc);
+   // GSL evaluate
+   double result;
+   switch(d) {
+     case none:
+       result = gsl_interp_eval(interpolator, &times[0], &points[0], time, acc);
+       break;
+     case first:
+       result = gsl_interp_eval_deriv(interpolator, &times[0], &points[0], time, acc);
+       break;
+     case second:
+       result = gsl_interp_eval_deriv2(interpolator, &times[0], &points[0], time, acc);
+       break;
+     default:
+       throw invalid_argument("Invalid Derivation option.");
+       break;
+   }
 
-    return result;
-  }
+   // GSL clean up
+   gsl_interp_free(interpolator);
+   gsl_interp_accel_free(acc);
 
-  double splineInterpolate(vector<double> points, vector<double> times, double time) {
-    size_t numPoints = points.size();
-    if (numPoints < 2) {
-      throw invalid_argument("Invalid interpolation data, at least two points are required to interpolate.");
-    }
-    if (points.size() != times.size()) {
-      throw invalid_argument("Invalid interpolation data, must have the same number of points as times.");
-    }
-    if (time < times.front() || time > times.back()) {
-      throw invalid_argument("Invalid interpolation time, outside of input times.");
-    }
-
-    // GSL setup
-    gsl_interp *interpolator = gsl_interp_alloc(gsl_interp_cspline, numPoints);
-    gsl_interp_init(interpolator, &times[0], &points[0], numPoints);
-    gsl_interp_accel *acc = gsl_interp_accel_alloc();
-
-    // GSL evaluate
-    double result = gsl_interp_eval(interpolator, &times[0], &points[0], time, acc);
-
-    // GSL clean up
-    gsl_interp_free(interpolator);
-    gsl_interp_accel_free(acc);
-
-    return result;
-  }
+   return result;
+ }
 
 }
