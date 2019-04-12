1f455daff474c851c9006ed6ff9ddb902663859b
Kelvin Rodriguez <kr788@nau.edu>
2019-04-12 07:18:23 -0700

    Updates to Kaguya Driver  (#99)
    * added Kaguya TC driver
    
    * addressed comments
    
    * forgot to import config
    
    * ijport glob
    
    * all the things
    
    * for some reason line_exposure_duration is a list, but only sometimes
    
    * updated Dawn with new bnames
    
    * better names for CTX Isis drivers(?)
    
    * renamed distortion
    
    * moved enter/exit logic back into the spice class
    
    * removed print statement
    
    * revert name change
    
    * tc_id -> _tc_id

--- ale/drivers/kaguya_driver.py
+++ ale/drivers/kaguya_driver.py
@@ -10,15 +10,32 @@ from glob import glob
 from ale import config
 from ale.drivers.base import Driver, LineScanner, PDS3, Spice, TransverseDistortion
 
-class TcPds3Driver(Driver, LineScanner, PDS3, Spice, TransverseDistortion):
+class TcPds3Driver(Driver, LineScanner, PDS3, Spice):
     @property
     def instrument_id(self):
-        instrument = self.label.get("INSTRUMENT_ID")
-        if instrument == "TC1":
-            return "LISM_TC1"
-        elif instrument == "TC2":
-            return "LISM_TC2"
+        """
+        Id takes the form of LISM_<INSTRUMENT_ID>_<SD><COMPRESS><SWATH> where
 
+        INSTRUMENT_ID = TC1/TC2
+        SD = S/D short for single or double, which in turn means whether the
+             the label belongs to a mono or stereo image.
+        COMPRESS = D/T short for DCT or through, we assume labels belong
+        """
+        instrument = self.label.get("INSTRUMENT_ID")
+        swath = self.label.get("SWATH_MODE_ID")[0]
+        sd = self.label.get("PRODUCT_SET_ID").split("_")[1].upper()
+
+        id = "LISM_{}_{}T{}".format(instrument, sd, swath)
+        return id
+
+    @property
+    def _tc_id(self):
+        """
+        Some keys are stored in the IK kernel under a general ikid for TC1/TC2
+        presumably because they are not affected by the addtional parameters encoded in
+        the ikid returned by self.ikid. This method exists for those gdpool calls.
+        """
+        return spice.bods2c("LISM_{}".format(self.label.get("INSTRUMENT_ID")))
 
     @property
     def metakernel(self):
@@ -31,12 +48,19 @@ class TcPds3Driver(Driver, LineScanner, PDS3, Spice, TransverseDistortion):
         return self._metakernel
 
     @property
+    def ending_ephemeris_time(self):
+        if not hasattr(self, '_ending_ephemeris_time'):
+            self._ending_ephemeris_time = self.label.get('CORRECTED_SC_CLOCK_STOP_COUNT').value
+            self._ending_ephemeris_time = spice.sct2e(self.spacecraft_id, self._ending_ephemeris_time)
+        return self._ending_ephemeris_time
+
+
+    @property
     def starting_ephemeris_time(self):
         if not hasattr(self, '_starting_ephemeris_time'):
             self._starting_ephemeris_time = self.label.get('CORRECTED_SC_CLOCK_START_COUNT').value
             self._starting_ephemeris_time = spice.sct2e(self.spacecraft_id, self._starting_ephemeris_time)
         return self._starting_ephemeris_time
-
 
     @property
     def _detector_center_line(self):
@@ -44,8 +68,7 @@ class TcPds3Driver(Driver, LineScanner, PDS3, Spice, TransverseDistortion):
 
     @property
     def _detector_center_sample(self):
-        return spice.gdpool('INS{}_CENTER'.format(self.ikid), 0, 2)[0]
-
+        return spice.gdpool('INS{}_CENTER'.format(self._tc_id), 0, 2)[0]
 
     @property
     def _sensor_orientation(self):
@@ -53,8 +76,9 @@ class TcPds3Driver(Driver, LineScanner, PDS3, Spice, TransverseDistortion):
             current_et = self.starting_ephemeris_time
             qua = np.empty((self.number_of_quaternions, 4))
             for i in range(self.number_of_quaternions):
+                instrument = self.label.get("INSTRUMENT_ID")
                 # Find the rotation matrix
-                camera2bodyfixed = spice.pxform("J2000",
+                camera2bodyfixed = spice.pxform("LISM_{}_HEAD".format(instrument),
                                                 self.reference_frame,
                                                 current_et)
                 q = spice.m2q(camera2bodyfixed)
@@ -64,42 +88,62 @@ class TcPds3Driver(Driver, LineScanner, PDS3, Spice, TransverseDistortion):
             self._orientation = qua
         return self._orientation.tolist()
 
-
     @property
-    def focal2pixel_lines(self):
+    def focal2pixel_samples(self):
         """
-        Calculated using pixel pitch and 1/pixel pitch
+        Calculated using 1/pixel pitch
         """
-        pixel_size = spice.gdpool('INS{}_PIXEL_SIZE'.format(self.ikid), 0, 1)[0]
+        pixel_size = spice.gdpool('INS{}_PIXEL_SIZE'.format(self._tc_id), 0, 1)[0]
         return [0, 0, 1/pixel_size]
 
 
     @property
-    def focal2pixel_samples(self):
+    def focal2pixel_lines(self):
         """
-        Calculated using pixel pitch and 1/pixel pitch
+        Calculated using 1/pixel pitch
         """
-        pixel_size = spice.gdpool('INS{}_PIXEL_SIZE'.format(self.ikid), 0, 1)[0]
+        pixel_size = spice.gdpool('INS{}_PIXEL_SIZE'.format(self._tc_id), 0, 1)[0]
         return [0, 1/pixel_size, 0]
 
 
     @property
-    def _odtx(self):
+    def _odkx(self):
         """
         Returns
         -------
         : list
           Optical distortion x coefficients
         """
-        return spice.gdpool('INS{}_DISTORTION_COEF_X'.format(self.ikid),0, 4).tolist()
+        return spice.gdpool('INS{}_DISTORTION_COEF_X'.format(self._tc_id),0, 4).tolist()
 
 
     @property
-    def _odty(self):
+    def _odky(self):
         """
         Returns
         -------
         : list
           Optical distortion y coefficients
         """
-        return spice.gdpool('INS{}_DISTORTION_COEF_Y'.format(self.ikid), 0, 4).tolist()
+        return spice.gdpool('INS{}_DISTORTION_COEF_Y'.format(self._tc_id), 0, 4).tolist()
+
+    @property
+    def line_exposure_duration(self):
+        # this is dumb
+        if isinstance(self.label['LINE_EXPOSURE_DURATION'], list):
+            return self.label['LINE_EXPOSURE_DURATION'][0].value * 0.001  # Scale to seconds
+        else:
+            return self.label['LINE_EXPOSURE_DURATION'].value * 0.001  # Scale to seconds
+
+    @property
+    def _focal_length(self):
+        return float(spice.gdpool('INS{}_FOCAL_LENGTH'.format(self._tc_id), 0, 1)[0])
+
+    @property
+    def optical_distortion(self):
+        return {
+            "kaguyatc": {
+                "x" : self._odky,
+                "y" : self._odkx
+            }
+        }
