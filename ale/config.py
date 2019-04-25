32cb4697a1a35dc9c739a0e27aba6a5dea5b4265
Kristin <kberry@usgs.gov>
2019-04-25 08:18:36 -0700

    Cassini ALE driver. (#110)
    * Add getPosition with coefficients
    
    * Add some exceptions and associated test
    
    * remove debug statements and change test name
    
    * Updated to use gsl for polynomial evaluation and separated out into a helper function. Swapped order of coeffs, other changes in response to review comments
    
    * Added a couple of suggested tests and updated documentation
    
    * Inital work toward ALE cassini driver
    
    * Updated comments in base.py and removed notebook from this PR
    
    * Update mk location to be accessible beyond my computer
    
    * remove newlines
    
    * Update name of Cassini ISS Ale driver
    
    * Update comment based on feedback

--- ale/config.py
+++ ale/config.py
@@ -4,8 +4,9 @@ Config File
 
 # Directory with metakernals
 spice_root = "/data/spice/"
-cassini = '/data/big/spice/co-s_j_e_v-spice-6-v1.0/cosp_1000/extras/mk'
+cassini = '/usgs/cpkgs/isis3/data/cassini/kernels/mk/' # Cassini ISS
 mdis = '/data/spice/mess-e_v_h-spice-6-v1.0/messsp_1000/extras/mk' # Messenger
 mro = '/data/spice/mro-m-spice-6-v1.0/mrosp_1000/extras/mk' # Mars Reconnaissance Orbiter
 kaguya = '/data/spice/SELENE/kernels/mk/'
 dawn = '/data/spice/dawn-m_a-spice-6-v1.0/dawnsp_1000/extras/mk'
+
