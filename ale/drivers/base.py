0bfba9574bf07857aa247cd99d5275b000de6288
Jesse Mapel <jmapel@usgs.gov>
2019-04-30 17:07:08 -0700

    spice driver passing tests

--- ale/drivers/base.py
+++ ale/drivers/base.py
@@ -13,6 +13,7 @@ from ale import config
 
 from scipy.interpolate import interp1d
 from scipy.spatial.transform import Slerp
+from scipy.spatial.transform import Rotation
 
 
 def read_table_data(table_label, cube):
@@ -142,8 +143,8 @@ def parse_rotation_table(label, field_data):
 
     Parameters
     ----------
-    table_label : PVLModule
-                  The table label
+    label : PVLModule
+            The table label
 
     field_data : dict
                  The table data as a dict with field names
@@ -168,6 +169,8 @@ def parse_rotation_table(label, field_data):
 
     if 'TimeDependentFrames' in label:
         results['TimeDependentFrames'] = np.array(label['TimeDependentFrames'])
+    if 'CkTableOriginalSize' in label:
+        results['CkTableOriginalSize'] = label['CkTableOriginalSize']
     if all (key in label for key in ('ConstantRotation','ConstantFrames')):
         const_rotation_mat = np.array(label['ConstantRotation'])
         results['ConstantRotation'] = np.reshape(const_rotation_mat, (3, 3))
@@ -180,18 +183,18 @@ def parse_rotation_table(label, field_data):
 
     return results
 
-def parse_position_table(field_data):
+def parse_position_table(label, field_data):
     """
     Parses ISIS position table data.
 
     Parameters
     ----------
-    table_label : PVLModule
-        The table label
+    label : PVLModule
+            The table label
 
     field_data : dict
-        The table data as a dict with field names as keys
-        and lists of entries as values
+                 The table data as a dict with field names as keys
+                 and lists of entries as values
 
     Returns
     -------
@@ -209,137 +212,9 @@ def parse_position_table(field_data):
         results['PositionCoefficients'] = np.array( [field_data['J2000SVX'][:-1],field_data['J2000SVY'][:-1],field_data['J2000SVZ'][:-1]] )
         results['BaseTime'] = field_data['J2000SVX'][-1]
         results['TimeScale'] = field_data['J2000SVY'][-1]
-    return results
 
-def parse_field(field_label, data, encoding='latin_1'):
-    """
-    Parses a binary table field entry and converts it into
-    an in memory data type
-
-    Parameters
-    ----------
-    field_label : PVLModule
-                  The field label
-
-    data : bytes
-           The binary data for the field entry
-
-    Returns
-    -------
-    Union[int, float, str, list] :
-        The table field entry converted to a native python
-        type
-    """
-    if field_label['Type'] == 'Text':
-        field_data = data[:field_label['Size']].decode(encoding=encoding)
-    else:
-        data_format = field_format(field_label)
-        field_data = struct.unpack_from(data_format, data)
-        if len(field_data) == 1:
-            field_data = field_data[0]
-    return field_data
-
-def parse_table_data(table_label, data):
-    """
-    Parses an ISIS table into a dict where the keys are the
-    field names and the values are lists of entries.
-
-    Parameters
-    ----------
-    table_label : PVLModule
-                  The table label
-
-    data : bytes
-           The binary data for the entire table
-
-    Returns
-    -------
-    dict :
-        The table as a dict
-    """
-    fields = table_label.getlist('Field')
-    results = {field['Name']:[] for field in fields}
-    offset = 0
-    for record in range(table_label['Records']):
-        for field in fields:
-            field_data = parse_field(field, data[offset:])
-            results[field['Name']].append(field_data)
-            offset += field_size(field)
-    return results
-
-def parse_rotation_table(label, field_data):
-    """
-    Parses ISIS rotation table data.
-
-    Parameters
-    ----------
-    table_label : PVLModule
-                  The table label
-
-    field_data : dict
-                 The table data as a dict with field names
-                 as keys and lists of entries as values
-
-    Returns
-    -------
-    dict :
-        The rotation data
-    """
-    results = {}
-    if all (key in field_data for key in ('J2000Q0','J2000Q1','J2000Q2','J2000Q3')):
-        results['Rotations'] = [ [q0, q1, q2, q3] for q0, q1, q2, q3 in zip(field_data['J2000Q0'],field_data['J2000Q1'],field_data['J2000Q2'],field_data['J2000Q3']) ]
-    if all (key in field_data for key in ('AV1','AV2','AV3')):
-        results['AngularVelocities'] = np.array( [ [av1, av2, av3] for av1, av2, av3 in zip(field_data['AV1'],field_data['AV2'],field_data['AV3']) ] )
-    if 'ET' in field_data:
-        results['Times'] = np.array(field_data['ET'])
-    if all (key in field_data for key in ('J2000Ang1','J2000Ang2','J2000Ang3')):
-        results['EulerCoefficients'] = np.array([field_data['J2000Ang1'],field_data['J2000Ang2'],field_data['J2000Ang3']])
-        results['BaseTime'] = field_data['J2000Ang1'][-1]
-        results['TimeScale'] = field_data['J2000Ang2'][-1]
-
-    if 'TimeDependentFrames' in label:
-        results['TimeDependentFrames'] = np.array(label['TimeDependentFrames'])
-    if all (key in label for key in ('ConstantRotation','ConstantFrames')):
-        const_rotation_mat = np.array(label['ConstantRotation'])
-        results['ConstantRotation'] = np.reshape(const_rotation_mat, (3, 3))
-        results['ConstantFrames'] = np.array(label['ConstantFrames'])
-    if all (key in label for key in ('PoleRa','PoleDec','PrimeMeridian')):
-        results['BodyRotationCoefficients'] = np.array( [label['PoleRa'],label['PoleDec'],label['PrimeMeridian']] )
-    if all (key in label for key in ('PoleRaNutPrec','PoleDecNutPrec','PmNutPrec','SysNutPrec0','SysNutPrec1')):
-        results['SatelliteNutationPrecessionCoefficients'] = np.array( [label['PoleRaNutPrec'],label['PoleDecNutPrec'],label['PmNutPrec']] )
-        results['PlanetNutationPrecessionAngleCoefficients'] = np.array( [label['SysNutPrec0'],label['SysNutPrec1']] )
-
-    return results
-
-def parse_position_table(field_data):
-    """
-    Parses ISIS position table data.
-
-    Parameters
-    ----------
-    table_label : PVLModule
-        The table label
-
-    field_data : dict
-        The table data as a dict with field names as keys
-        and lists of entries as values
-
-    Returns
-    -------
-    dict :
-      The position data
-    """
-    results = {}
-    if all (key in field_data for key in ('J2000X','J2000Y','J2000Z')):
-        results['Positions'] = np.array( [ [x, y, z] for x, y, z in zip(field_data['J2000X'],field_data['J2000Y'],field_data['J2000Z']) ] )
-    if 'ET' in field_data:
-        results['Times'] = np.array(field_data['ET'])
-    if all (key in field_data for key in ('J2000XV','J2000YV','J2000ZV')):
-        results['Velocities'] = np.array( [ [x, y, z] for x, y, z in zip(field_data['J2000XV'],field_data['J2000YV'],field_data['J2000ZV']) ] )
-    if all (key in field_data for key in ('J2000SVX','J2000SVY','J2000SVZ')):
-        results['PositionCoefficients'] = np.array( [field_data['J2000SVX'][:-1],field_data['J2000SVY'][:-1],field_data['J2000SVZ'][:-1]] )
-        results['BaseTime'] = field_data['J2000SVX'][-1]
-        results['TimeScale'] = field_data['J2000SVY'][-1]
+    if 'SpkTableOriginalSize' in label:
+        results['SpkTableOriginalSize'] = label['SpkTableOriginalSize']
     return results
 
 
@@ -713,11 +588,11 @@ class Pds3Label():
 
     @property
     def _exposure_duration(self):
-        # The EXPOSURE_DURATION may either be stored as a (value, unit) or just a value 
+        # The EXPOSURE_DURATION may either be stored as a (value, unit) or just a value
         try:
             return self.label['EXPOSURE_DURATION'].value * 0.001
         except:
-            return self.label['EXPOSURE_DURATION'] * 0.001  
+            return self.label['EXPOSURE_DURATION'] * 0.001
 
 
 class NaifSpice():
@@ -1031,9 +906,9 @@ class IsisSpice(IsisLabel):
             elif table['Name'] == 'BodyRotation':
                 self._body_orientation_table = parse_rotation_table(table, field_data)
             elif table['Name'] == 'InstrumentPosition':
-                self._inst_position_table = parse_position_table(field_data)
+                self._inst_position_table = parse_position_table(table, field_data)
             elif table['Name'] == 'SunPosition':
-                self._sun_position_table = parse_position_table(field_data)
+                self._sun_position_table = parse_position_table(table, field_data)
 
     @property
     def label(self):
@@ -1077,7 +952,9 @@ class IsisSpice(IsisLabel):
         int :
             The number of quaternions
         """
-        return self._num_ephem
+        if not hasattr(self, "_inst_pointing_table"):
+            self._init_tables()
+        return self._inst_pointing_table['CkTableOriginalSize']
 
     @property
     def number_of_ephemerides(self):
@@ -1090,7 +967,9 @@ class IsisSpice(IsisLabel):
         int :
             The number of states
         """
-        return self._num_quaternions
+        if not hasattr(self, "_inst_position_table"):
+            self._init_tables()
+        return self._inst_position_table['SpkTableOriginalSize']
 
     @property
     def _sclock_hex_string(self):
@@ -1245,7 +1124,7 @@ class IsisSpice(IsisLabel):
             semiminor
         """
         for key in self.naif_keywords:
-            if re.match('BODY-?\d*_RADII', key[0]):
+            if re.match(r'BODY-?\d*_RADII', key[0]):
                 return self.naif_keywords[key[0]]
 
     @property
@@ -1343,20 +1222,30 @@ class IsisSpice(IsisLabel):
         if not hasattr(self, "_inst_position_table"):
             self._init_tables()
 
-        inst_positions_times = cube._inst_position_table["Times"]
-        inst_positions_times = np.linspace(inst_positions_times[0], inst_positions_times[-1], self.number_of_ephemerides)
+        inst_positions_times = np.linspace(self._inst_position_table["Times"][0],
+                                           self._inst_position_table["Times"][-1],
+                                           int(self.number_of_ephemerides))
 
         # interpolate out positions
-        f_positions_x = interp1d(cube._inst_position_table["Times"], cube._inst_position_table["Positions"].T[0])
-        f_positions_y = interp1d(cube._inst_position_table["Times"], cube._inst_position_table["Positions"].T[1])
-        f_positions_z = interp1d(cube._inst_position_table["Times"], cube._inst_position_table["Positions"].T[2])
+        if len(self._inst_position_table["Times"]) < 2:
+            time_0 = self._inst_position_table["Times"][0]
+            position_0 = self._inst_position_table["Positions"][0]
+            velocity_0 = self._inst_position_table["Velocities"][0]
+            coefs = np.asarray([position_0 - time_0*velocity_0,
+                                velocity_0])
+            positions = np.polynomial.polynomial.polyval(inst_positions_times, coefs)
 
-        positions = np.asarray([f_positions_x(inst_positions_times),
-                               f_positions_y(inst_positions_times),
-                               f_positions_z(inst_positions_times)]).T
+        else:
+            f_positions_x = interp1d(self._inst_position_table["Times"], self._inst_position_table["Positions"].T[0])
+            f_positions_y = interp1d(self._inst_position_table["Times"], self._inst_position_table["Positions"].T[1])
+            f_positions_z = interp1d(self._inst_position_table["Times"], self._inst_position_table["Positions"].T[2])
+
+            positions = np.asarray([f_positions_x(inst_positions_times),
+                                   f_positions_y(inst_positions_times),
+                                   f_positions_z(inst_positions_times)])
 
         # convert positions to Body-Fixed and scale to meters
-        return self._body_j2k2bf_rotation.apply(positions)*1000
+        return self._body_j2k2bf_rotation.apply(positions.T)*1000
 
 
     @property
@@ -1373,20 +1262,27 @@ class IsisSpice(IsisLabel):
         if not hasattr(self, "_inst_position_table"):
             self._init_tables()
 
-        inst_velocities_times = cube._inst_position_table["Times"]
-        inst_velocities_times = np.linspace(inst_velocities_times[0], inst_velocities_times[-1], self.number_of_ephemerides)
+        inst_velocities_times = np.linspace(self._inst_position_table["Times"][0],
+                                           self._inst_position_table["Times"][-1],
+                                           int(self.number_of_ephemerides))
 
-        # interpolate out positions
-        f_velocities_x = interp1d(cube._inst_position_table["Times"], cube._inst_position_table["Velocities"].T[0])
-        f_velocities_y = interp1d(cube._inst_position_table["Times"], cube._inst_position_table["Velocities"].T[1])
-        f_velocities_z = interp1d(cube._inst_position_table["Times"], cube._inst_position_table["Velocities"].T[2])
+        if len(self._inst_position_table["Times"]) < 2:
+            velocity_0 = self._inst_position_table["Velocities"][0]
+            coefs = np.asarray([velocity_0,
+                                [0, 0, 0]])
+            velocties = np.polynomial.polynomial.polyval(inst_velocities_times, coefs)
 
-        velocties = np.asarray([f_velocities_x(inst_velocities_times),
-                               f_velocities_y(inst_velocities_times),
-                               f_velocities_z(inst_velocities_times)]).T
+        else:
+            f_velocities_x = interp1d(self._inst_position_table["Times"], self._inst_position_table["Velocities"].T[0])
+            f_velocities_y = interp1d(self._inst_position_table["Times"], self._inst_position_table["Velocities"].T[1])
+            f_velocities_z = interp1d(self._inst_position_table["Times"], self._inst_position_table["Velocities"].T[2])
+
+            velocties = np.asarray([f_velocities_x(inst_velocities_times),
+                                   f_velocities_y(inst_velocities_times),
+                                   f_velocities_z(inst_velocities_times)])
 
         # convert positions to Body-Fixed and scale to meters
-        return self._body_j2k2bf_rotation.apply(velocties)*1000
+        return self._body_j2k2bf_rotation.apply(velocties.T)*1000
 
     @property
     def _sensor_orientation(self):
@@ -1403,15 +1299,20 @@ class IsisSpice(IsisLabel):
         if not hasattr(self, "_inst_pointing_table"):
             self._init_tables()
 
-        inst_pointing_times = cube._inst_pointing_table["Times"]
-        inst_pointing_times = np.linspace(inst_pointing_times[0], inst_pointing_times[-1], self.number_of_quaternions)
-        rotations = cube._inst_pointing_table["Rotations"]
+        inst_pointing_times = np.linspace(self._inst_pointing_table["Times"][0],
+                                          self._inst_pointing_table["Times"][-1],
+                                          int(self.number_of_quaternions))
+        rotations = self._inst_pointing_table["Rotations"]
         rotations = np.roll(np.asarray(rotations), -1, 1) # adjust rotations [0,1,2,3] -> [3,0,1,2]
 
-        # interpolate out orientation
-        orientations = Slerp(cube._inst_pointing_table["Times"], R.from_quat(rotations))(inst_pointing_times)
+        if len(self._inst_pointing_table["Times"]) < 2:
+            orientations = Rotation.from_quat(rotations[0])
+        else:
+            orientations = Slerp(self._inst_pointing_table["Times"], Rotation.from_quat(rotations))(inst_pointing_times)
 
-        return (self._body_j2k2bf_rotation*self._inst2bf_rotation.inv()*orientations).as_quat()
+        print('instrument pointing', orientations.as_dcm())
+        bf2inst_rotation = (orientations*self._body_j2k2bf_rotation.inv()).as_quat()
+        return bf2inst_rotation
 
     @property
     def body_orientation(self):
@@ -1452,47 +1353,34 @@ class IsisSpice(IsisLabel):
     @property
     def _body_j2k2bf_rotation(self):
         """
-        Returns Mt*Mc where:
-        Mt is the time dependant rotation from j2000 to body fixed
-        Mc is contant rotation from J2000 to body fixed.
+        Returns Mc*Mt where:
+        Mt is the time dependant portion of the rotation from j2000 to body fixed
+        Mc is contant portion of the rotation from J2000 to body fixed.
 
         This represents the rotation to get positions from J2000 to body fixed,
         """
-        if not hasattr(self, "_inst_position_table"):
+        if not hasattr(self, "_body_orientation_table"):
             self._init_tables()
 
-        body_rot_times = cube._body_orientation_table["Times"]
-        body_const_rots = cube._body_orientation_table["ConstantRotation"]
-        body_timed_rots = cube._body_orientation_table["Rotations"]
-        body_timed_rots = np.roll(np.asarray(body_timed_rots), -1, 1) # adjust rotations [0,1,2,3] -> [3,0,1,2]
+        body_rot_times = self._body_orientation_table["Times"]
+        body_timed_rots = self._body_orientation_table["Rotations"]
+        body_timed_rots = np.roll(np.asarray(body_timed_rots), -1, 1) # adjust quaternions [0,1,2,3] -> [3,0,1,2]
 
-        interp_rot_times = np.linspace(body_rot_times[0], body_rot_times[-1], self.number_of_ephemerides)
+        interp_rot_times = np.linspace(body_rot_times[0],
+                                       body_rot_times[-1],
+                                       int(self.number_of_ephemerides))
 
-        body_timed_rots = Slerp(body_rot_times, R.from_quat(body_timed_rots))(interp_rot_times)
+        if len(self._body_orientation_table["Times"]) < 2:
+            rotation_mat = Rotation.from_quat(body_timed_rots[0])
+        else:
+            rotation_mat = Slerp(body_rot_times, Rotation.from_quat(body_timed_rots))(interp_rot_times)
 
-        # Matrix to convert j2000 positions to body fixed
-        return body_timed_rots*R.from_dcm(body_const_rots)
+        # Not all body rotations have a constant component
+        if "ConstantRotation" in self._body_orientation_table:
+            body_const_rots = self._body_orientation_table["ConstantRotation"]
+            rotation_mat = Rotation.from_dcm(body_const_rots)*rotation_mat
 
-
-    @property
-    def _inst2bf_rotation(self):
-        """
-        """
-
-        if not hasattr(self, "_inst_position_table"):
-            self._init_tables()
-
-        inst_rot_times = cube._inst_pointing_table["Times"]
-        inst_const_rots = cube._inst_pointing_table["ConstantRotation"]
-        inst_timed_rots = cube._inst_pointing_table["Rotations"]
-        inst_timed_rots = np.roll(np.asarray(inst_timed_rots), -1, 1) # adjust rotations [0,1,2,3] -> [3,0,1,2]
-
-        interp_rot_times = np.linspace(inst_rot_times[0], inst_rot_times[-1], self.number_of_quaternions)
-
-        inst_timed_rots = Slerp(inst_rot_times, R.from_quat(inst_timed_rots))(interp_rot_times)
-
-        # Matrix to convert instrument pointing to j2000
-        return inst_timed_rots*R.from_dcm(inst_const_rots)
+        return rotation_mat
 
 
 class RadialDistortion():
