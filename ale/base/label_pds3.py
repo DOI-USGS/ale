bef274ea476d7388703d6d4c444bbe1eb2e842a4
Stuart Sides <ssides@usgs.gov>
2019-05-10 13:24:01 -0700

    Update to new design

--- ale/base/label_pds3.py
+++ ale/base/label_pds3.py
@@ -13,35 +13,38 @@ class Pds3Label():
     """
 
     @property
-    def _focal_plane_tempature(self):
-        return self.label['FOCAL_PLANE_TEMPERATURE'].value
+    """
+    Returns the number of lines in the image.
 
-    @property
-    def instrument_id(self):
-        pass
-
-    @property
-    def start_time(self):
-        return self.label['START_TIME']
-
-    @property
+    Returns
+    -------
+    : int
+      number of lines
+    """
     def image_lines(self):
         return self.label['IMAGE']['LINES']
 
+
     @property
+    """
+    Returns the number of samples in the image.
+
+    Returns
+    -------
+    : int
+      number of samples
+    """
     def image_samples(self):
         return self.label['IMAGE']['LINE_SAMPLES']
 
-    @property
-    def interpolation_method(self):
-        return 'lagrange'
 
     @property
     def target_name(self):
         """
-        Returns an target name for unquely identifying the instrument, but often
-        piped into Spice Kernels to acquire Ephermis data from Spice. Therefore they
-        the same ID the Spice expects in bodvrd calls.
+        Returns a target name unquely identifying what an observation was capturing. 
+        This is most often a body name (e.g., Mars, Moon, Europa). This value is often
+        use to acquire Ephermis data from SPICE files; therefore it should be the same
+        ID spicelib expects in bodvrd calls.
 
         Returns
         -------
@@ -51,53 +54,66 @@ class Pds3Label():
 
         return self.label['TARGET_NAME']
 
-    @property
-    def _target_id(self):
-        return spice.bodn2c(self.label['TARGET_NAME'])
 
     @property
-    def starting_ephemeris_time(self):
-        if not hasattr(self, '_starting_ephemeris_time'):
-            sclock = self.label['SPACECRAFT_CLOCK_START_COUNT']
-            self._starting_ephemeris_time = spice.scs2e(self.spacecraft_id, sclock)
-        return self._starting_ephemeris_time
+    """
+    Returns the summing factor from the PDS3 label. For example a return value of 2 
+    indicates that 2 lines and 2 samples (4 pixels) were summed and divided by 4
+    to produce the output pixel value.
 
-    @property
-    def spacecraft_clock_stop_count(self):
-        sc = self.label.get('SPACECRAFT_CLOCK_STOP_COUNT', None)
-        if sc == 'N/A':
-            sc = None
-        return sc
-
-    @property
-    def _detector_center_line(self):
-        return spice.gdpool('INS{}_CCD_CENTER'.format(self.ikid), 0, 2)[0]
-
-    @property
-    def _detector_center_sample(self):
-        return spice.gdpool('INS{}_CCD_CENTER'.format(self.ikid), 0, 2)[1]
-
-    @property
-    def spacecraft_name(self):
-        """
-        Spacecraft name used in various Spice calls to acquire
-        ephemeris data.
-
-        Returns
-        -------
-        : str
-          Spacecraft name
-        """
-        return self.label['MISSION_NAME']
-
-    @property
-    def detector_line_summing(self):
+    Returns
+    -------
+    : int
+      number of samples and lines combined from the original data to produce a single pixel in this image
+    """
+    def detector_summing(self):
         return self.label.get('SAMPLING_FACTOR', 1)
 
+
     @property
-    def _exposure_duration(self):
+    """
+    Returns
+    -------
+    : str
+      Returns the start clock count string from the PDS3 label.
+    """
+    def spacecraft_start_clock_count(self):
+        return self.label['SPACECRAFT_CLOCK_START_COUNT']
+
+
+    @property
+    """
+    Returns
+    -------
+    : str
+      Returns the stop clock count string from the PDS3 label.
+    """
+    def spacecraft_stop_clock_count(self):
+        count = self.label['SPACECRAFT_CLOCK_STOP_COUNT', None]
+        if count == 'N/A':
+            count = None
+        return count
+
+
+    @property
+    """
+     Returns
+     -------
+     : float
+       Returns the exposure duration in seconds from the PDS3 label.
+     """
+    def exposure_duration(self):
         # The EXPOSURE_DURATION may either be stored as a (value, unit) or just a value
         try:
-            return self.label['EXPOSURE_DURATION'].value * 0.001
+            unit = self.label['EXPOSURE_DURATION'].unit.lower()
+            if unit == "ms":
+              return self.label['EXPOSURE_DURATION'].value * 0.001
+            else:
+              return self.label['EXPOSURE_DURATION'].value
+
+        # With no units, assume seconds
         except:
             return self.label['EXPOSURE_DURATION'] * 0.001
+
+
+
