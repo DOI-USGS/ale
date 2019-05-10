0f84dff66eb2b7faaee2719e89735b5820ec89c3
Kristin <kberry@usgs.gov>
2019-05-10 13:02:00 -0700

    clean up distortion test

--- tests/pytests/test_distortion.py
+++ tests/pytests/test_distortion.py
@@ -3,10 +3,10 @@ import pvl
 
 import ale
 from ale import base
-from ale.base import type_distortion
+from ale.base.type_distortion import RadialDistortion
 
 def test_radial_distortion():
-    m = base.type_distortion.RadialDistortion()
-    m._odtk = [0.0, 0.1, 0.2]
-    assert m.optical_distortion["radial"]["coefficients"] == [0.0, 0.1, 0.2]
+    radial_distortion = RadialDistortion()
+    radial_distortion._odtk = [0.0, 0.1, 0.2]
+    assert radial_distortion.optical_distortion["radial"]["coefficients"] == [0.0, 0.1, 0.2]
 
