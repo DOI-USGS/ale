3e9581e79d234df48ac92c0b68c451fe865f683a
Peter Giroux <pgiroux@contractor.usgs.gov>
2019-05-29 08:46:21 -0700

    changed mocking to improve readability

--- tests/pytests/test_dawn_drivers.py
+++ tests/pytests/test_dawn_drivers.py
@@ -24,17 +24,14 @@ DawnFcPds3NaifSpiceDriver.metakernel = get_mockkernels
 def driver():
     return DawnFcPds3NaifSpiceDriver("")
 
+@patch('ale.base.label_pds3.Pds3Label.instrument_id', 1)
+@patch('ale.base.label_pds3.Pds3Label.filter_number', 2)
 def test_instrument_id(driver):
-    with patch('ale.base.label_pds3.Pds3Label.instrument_id', new_callable=PropertyMock) as mock_instrument_id:
-        mock_instrument_id.return_value = 1
-        with patch('ale.base.label_pds3.Pds3Label.filter_number', new_callable=PropertyMock) as mock_filter_number:
-            mock_filter_number.return_value = 2
-            assert driver.instrument_id == 'DAWN_1_FILTER_2'
+    assert driver.instrument_id == 'DAWN_1_FILTER_2'
 
+@patch('ale.base.label_pds3.Pds3Label.instrument_host_id', 'DAWN')
 def test_spacecraft_name(driver):
-    with patch('ale.base.label_pds3.Pds3Label.instrument_host_id', new_callable=PropertyMock) as mock_host_id:
-        mock_host_id.return_value = 'DAWN'
-        assert driver.spacecraft_name == 'DAWN'
+    assert driver.spacecraft_name == 'DAWN'
 
 def test_target_name(driver):
     with patch('ale.base.label_pds3.Pds3Label.target_name', new_callable=PropertyMock) as mock_target_name:
@@ -43,33 +40,27 @@ def test_target_name(driver):
         mock_target_name.return_value = 'VESTA'
         assert driver.target_name == 'VESTA'
 
+@patch('ale.base.label_pds3.Pds3Label.spacecraft_clock_start_count', 123)
+@patch('ale.base.label_pds3.Pds3Label.instrument_host_id', 'DAWN')
 def test_ephemeris_start_time(driver):
-    with patch('ale.base.label_pds3.Pds3Label.spacecraft_clock_start_count', new_callable=PropertyMock) as mock_start_count:
-        mock_start_count.return_value == 123
-        with patch('ale.base.label_pds3.Pds3Label.instrument_host_id', new_callable=PropertyMock) as mock_host_id:
-            mock_host_id.return_value = 'DAWN'
-            assert driver.ephemeris_start_time == .1 + 193.0 / 1000.0
+    assert driver.ephemeris_start_time == .1 + 193.0 / 1000.0
 
+@patch('ale.base.data_naif.NaifSpice.ikid', 123)
 def test_odtk(driver):
-    with patch('ale.base.data_naif.NaifSpice.ikid', new_callable=PropertyMock) as mock_ikid:
-        mock_ikid.return_value = 123
-        assert driver.odtk == [1]
+    assert driver.odtk == [1]
 
+@patch('ale.base.data_naif.NaifSpice.ikid', 123)
 def test_usgscsm_distortion_model(driver):
-    with patch('ale.base.data_naif.NaifSpice.ikid', new_callable=PropertyMock) as mock_ikid:
-        mock_ikid.return_value = 123
-        od = driver.usgscsm_distortion_model
-        assert od['dawnfc']['coefficients'] == [1]
+    distortion_model = driver.usgscsm_distortion_model
+    assert distortion_model['dawnfc']['coefficients'] == [1]
 
+@patch('ale.base.data_naif.NaifSpice.ikid', 123)
 def test_focal2pixel_samples(driver):
-    with patch('ale.base.data_naif.NaifSpice.ikid', new_callable=PropertyMock) as mock_ikid:
-        mock_ikid.return_value = 123
-        assert driver.focal2pixel_samples == [0, 1000, 0]
+    assert driver.focal2pixel_samples == [0, 1000, 0]
 
+@patch('ale.base.data_naif.NaifSpice.ikid', 123)
 def test_focal2pixel_lines(driver):
-    with patch('ale.base.data_naif.NaifSpice.ikid', new_callable=PropertyMock) as mock_ikid:
-        mock_ikid.return_value = 123
-        assert driver.focal2pixel_lines == [0, 0, 1000]
+    assert driver.focal2pixel_lines == [0, 0, 1000]
 
 def test_sensor_model_version(driver):
     assert driver.sensor_model_version == 2
