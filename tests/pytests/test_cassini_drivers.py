1786593723877c509a6821b9d50fc2f8a4a57279
paarongiroux <47163875+paarongiroux@users.noreply.github.com>
2019-05-30 14:02:44 -0700

    changed cassini tests to follow new format (#173)

--- tests/pytests/test_cassini_drivers.py
+++ tests/pytests/test_cassini_drivers.py
@@ -26,48 +26,39 @@ def driver():
 
 def test_instrument_id(driver):
     with patch('ale.base.label_pds3.Pds3Label.instrument_id', new_callable=PropertyMock) as mock_instrument_id:
-
         mock_instrument_id.return_value = 'ISSNA'
         assert driver.instrument_id == 'CASSINI_ISS_NAC'
-
         mock_instrument_id.return_value = 'ISSWA'
         assert driver.instrument_id == 'CASSINI_ISS_WAC'
 
+@patch('ale.base.data_naif.NaifSpice.ikid', 123)
 def test_focal_epsilon(driver):
-    with patch('ale.base.data_naif.NaifSpice.ikid', new_callable=PropertyMock) as mock_ikid:
-        mock_ikid.return_value = 123
-        assert driver.focal_epsilon == 1
+    assert driver.focal_epsilon == 1
 
 def test_spacecraft_name(driver):
     assert driver.spacecraft_name == 'CASSINI'
 
+@patch('ale.base.data_naif.NaifSpice.ikid', 123)
 def test_focal2pixel_samples(driver):
-    with patch('ale.base.data_naif.NaifSpice.ikid', new_callable=PropertyMock) as mock_ikid:
-        mock_ikid.return_value = 123
-        assert driver.focal2pixel_samples == [0,1000,0]
+    assert driver.focal2pixel_samples == [0,1000,0]
 
+@patch('ale.base.data_naif.NaifSpice.ikid', 123)
 def test_focal2pixel_lines(driver):
-    with patch('ale.base.data_naif.NaifSpice.ikid', new_callable=PropertyMock) as mock_ikid:
-        mock_ikid.return_value = 123
-        assert driver.focal2pixel_lines == [0,0,1000]
+    assert driver.focal2pixel_lines == [0,0,1000]
 
 def testodtk(driver):
     with patch('ale.base.label_pds3.Pds3Label.instrument_id', new_callable=PropertyMock) as mock_instrument_id:
-
         mock_instrument_id.return_value = 'ISSNA'
         assert driver.odtk == [float('-8e-6'), 0, 0]
-
         mock_instrument_id.return_value = 'ISSWA'
         assert driver.odtk == [float('-6.2e-5'), 0, 0]
 
+@patch('ale.base.data_naif.NaifSpice.ikid', 123)
 def test_detector_center_line(driver):
-    with patch('ale.base.data_naif.NaifSpice.ikid', new_callable=PropertyMock) as mock_ikid:
-        mock_ikid.return_value = 123
-        assert driver.detector_center_line == 1
+    assert driver.detector_center_line == 1
 
+@patch('ale.base.data_naif.NaifSpice.ikid', 123)
 def test_detector_center_sample(driver):
-    with patch('ale.base.data_naif.NaifSpice.ikid', new_callable=PropertyMock) as mock_ikid:
-        mock_ikid.return_value = 123
         assert driver.detector_center_sample == 1
 
 def test_sensor_model_version(driver):
