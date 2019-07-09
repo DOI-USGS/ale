29e1b2636f5541915bdb6b0d5827c2c3c975ca2e
Kristin <kberry@usgs.gov>
2019-07-09 11:21:14 -0700

    Fix cassini driver start time keyword

--- ale/drivers/cassini_drivers.py
+++ ale/drivers/cassini_drivers.py
@@ -33,11 +33,11 @@ class CassiniIssPds3LabelNaifSpiceDriver(Pds3Label, NaifSpice, Framer, RadialDis
           Path to latest metakernel file
         """
         metakernel_dir = config.cassini
-
+        print(metakernel_dir)
         mks = sorted(glob(os.path.join(metakernel_dir,'*.tm')))
         if not hasattr(self, '_metakernel'):
             for mk in mks:
-               if str(self.start_time.year) in os.path.basename(mk):
+               if str(self.utc_start_time.year) in os.path.basename(mk):
                    self._metakernel = mk
         return self._metakernel
 
