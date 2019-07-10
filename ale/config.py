08014ab494503806783bf93d0148434a27e86d9b
Kelvin Rodriguez <kr788@nau.edu>
2019-07-10 12:22:42 -0700

    updated version number  (#200)
    * version tick
    
    * reverted config

--- ale/config.py
+++ ale/config.py
@@ -9,5 +9,4 @@ mdis = '/data/spice/mess-e_v_h-spice-6-v1.0/messsp_1000/extras/mk' # Messenger
 mro = '/data/spice/mro-m-spice-6-v1.0/mrosp_1000/extras/mk' # Mars Reconnaissance Orbiter
 kaguya = '/data/spice/SELENE/kernels/mk/'
 dawn = '/data/spice/dawn-m_a-spice-6-v1.0/dawnsp_1000/extras/mk'
-lro = '/usgs/cpkgs/isis3/data/lro/kernels/mk/' # LRO 
-
+lro = '/usgs/cpkgs/isis3/data/lro/kernels/mk/' # LRO
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup, find_packages
 
 NAME = "Ale"
-VERSION = "0.0.3"
+VERSION = "0.2.0"
 
 # To install the library, run the following
 #
