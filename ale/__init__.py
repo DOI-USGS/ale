d40ed9133ecbff10a72a6aa343557fd1ea8c9a47
Kelvinrr <Kelvinrr@icloud.com>
2019-07-10 11:54:34 -0700

    version tick

--- ale/__init__.py
+++ ale/__init__.py
@@ -1,3 +1,21 @@
 from . import drivers
 from . import formatters
 from .drivers import load, loads
+
+import os
+
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    _dist = get_distribution('ale')
+    # Normalize case for Windows systems
+    dist_loc = os.path.normcase(_dist.location)
+    here = os.path.normcase(__file__)
+    if not here.startswith(os.path.join(dist_loc, 'ale')):
+        # not installed, but there is another version that *is*
+        raise DistributionNotFound
+except DistributionNotFound:
+    __version__ = 'Please install this project with setup.py'
+else:
+    __version__ = _dist.version
+
