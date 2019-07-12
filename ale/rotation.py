Cassini ISS - rotation fix? (#201)
* Updated history

* Small fixes based on feedback

* Small fixes based on feedback

ale/rotation.py
@@ -163,7 +163,7 @@ class TimeDependentRotation:
 
         The destination frame of the right rotation (other) and the source
         frame of the left rotation (self) must be the same. I.E. if A and B are
-        rotations, then for A*B to be valid, A.source must equal B.dest.
+        rotations, then for A*B to be valid, A.source must equal B.dest. 
 
         If the other rotation is a time dependent rotation, then the time range
         for the resultant rotation will be the time covered by both rotations.
@@ -179,6 +179,9 @@ class TimeDependentRotation:
         if isinstance(other, ConstantRotation):
             return TimeDependentRotation((self._rots * other._rot).as_quat(), self.times, other.source, self.dest)
         elif isinstance(other, TimeDependentRotation):
+            # if self and other each have the same time and one rotation, don't interpolate.
+            if (self.times.size == 1) and (other.times.size == 1) and (self.times == other.times):
+                return TimeDependentRotation((self._rots * other._rots).as_quat(), self.times, other.source, self.dest)
             merged_times = np.union1d(np.asarray(self.times), np.asarray(other.times))
             # we cannot extrapolate so clip to the time range both cover
             first_time = max(min(self.times), min(other.times))