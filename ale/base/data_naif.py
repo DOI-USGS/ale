c82fbf0b90a3a388feb3b16c51da8968643d4f9a
Jesse Mapel <jmapel@usgs.gov>
2019-07-22 14:51:38 -0700

    Adds simple light time correction (#219)
    * Added light time property to data_naif
    
    * Changed default to LT+S to match ISIS. Updated LRO to NONE

--- ale/base/data_naif.py
+++ ale/base/data_naif.py
@@ -27,6 +27,25 @@ class NaifSpice():
         pass
 
     @property
+    def light_time_correction(self):
+        """
+        Returns the type of light time correciton and abberation correction to
+        use in NAIF calls.
+
+        This defaults to light time correction and abberation correction (LT+S),
+        concrete drivers should override this if they need to either not use
+        light time correction or use a different type of light time correction.
+
+        Returns
+        -------
+        : str
+          The light time and abberation correction string for use in NAIF calls.
+          See https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/req/abcorr.html
+          for the different options available.
+        """
+        return 'LT+S'
+
+    @property
     def odtx(self):
         """
         Returns the x coefficient for the optical distortion model
@@ -254,7 +273,7 @@ class NaifSpice():
         sun_state, _ = spice.spkezr("SUN",
                                      self.center_ephemeris_time,
                                      self.reference_frame,
-                                     'NONE',
+                                     self.light_time_correction,
                                      self.target_name)
 
         return [sun_state[:4].tolist()], [sun_state[3:6].tolist()], [self.center_ephemeris_time]
@@ -283,7 +302,7 @@ class NaifSpice():
                 state, _ = spice.spkezr(self.spacecraft_name,
                                         time,
                                         self.reference_frame,
-                                        'NONE',
+                                        self.light_time_correction,
                                         self.target_name,)
                 pos.append(state[:3])
                 vel.append(state[3:])
