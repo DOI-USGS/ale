0ee8a8165f97426ca061179472fc50031e0e0081
Adam Paquette <acpaquette@usgs.gov>
2019-09-03 14:33:01 -0700

    Simplified load

--- tests/pytests/test_mro_drivers.py
+++ tests/pytests/test_mro_drivers.py
@@ -125,15 +125,16 @@ def isis_compare_dict():
 @pytest.fixture()
 def test_kernels():
     kernels = get_image_kernels('B10_013341_1010_XN_79S172W')
-    print(kernels)
     updated_kernels, binary_kernels = convert_kernels(kernels)
     yield updated_kernels
     for kern in binary_kernels:
         os.remove(kern)
 
-@pytest.mark.parametrize("label_type", ['pds3', 'isis3'])
-@pytest.mark.parametrize("formatter", ['usgscsm', 'isis'])
-def test_mro_load(test_kernels, label_type, formatter, usgscsm_compare_dict, isis_compare_dict):
+# @pytest.mark.parametrize("label_type", ['pds3', 'isis3'])
+# @pytest.mark.parametrize("formatter", ['usgscsm', 'isis'])
+def test_mro_load(test_kernels, usgscsm_compare_dict, isis_compare_dict):
+    label_type='pds3'
+    formatter='usgscsm'
     label_file = get_image_label('B10_013341_1010_XN_79S172W', label_type)
 
     usgscsm_isd_str = ale.loads(label_file, props={'kernels': test_kernels}, formatter=formatter)
