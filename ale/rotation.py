Fixed velocity rotation

ale/rotation.py
@@ -366,7 +366,7 @@ class TimeDependentRotation:
             skew = np.array([[0, -avs[indx, 2], avs[indx, 1]],
                              [avs[indx, 2], 0, -avs[indx, 0]],
                              [-avs[indx, 1], avs[indx, 0], 0]])
-            rot_deriv = np.dot(skew, rots[indx].as_dcm())
+            rot_deriv = np.dot(skew, rots[indx].as_dcm().T).T
             rotated_vel[indx] = rots[indx].apply(vec_vel[indx])
             rotated_vel[indx] += np.dot(rot_deriv, vec_pos[indx])
 

ale/transformation.py
@@ -110,7 +110,7 @@ class FrameChain(nx.DiGraph):
             avs = np.zeros((len(times), 3))
             for j, time in enumerate(times):
                 state_matrix = spice.sxform(spice.frmnam(s), spice.frmnam(d), time)
-                rotation_matrix, avs[i] = spice.xf2rav(state_matrix)
+                rotation_matrix, avs[j] = spice.xf2rav(state_matrix)
                 quat_from_rotation = spice.m2q(rotation_matrix)
                 quats[j,:3] = quat_from_rotation[1:]
                 quats[j,3] = quat_from_rotation[0]

tests/pytests/test_rotation.py
@@ -230,6 +230,6 @@ def test_rotate_velocity_at():
     input_times = [1, 2, 3]
     rot_vel = rot.rotate_velocity_at(input_pos, input_vel, input_times)
     np.testing.assert_almost_equal(rot_vel,
-                                   [[-1, -3 - np.pi, 2 - 3*np.pi/2],
-                                    [1 + 2*np.pi, -3, -2 + np.pi],
-                                    [3 + np.pi/2, 1 - 3*np.pi/2, -2]])
+                                   [[-1, -3 + np.pi, 2 + 3*np.pi/2],
+                                    [1 + 3*np.pi, -3 + np.pi, -2],
+                                    [3, 1 - np.pi, -2 - np.pi/2]])