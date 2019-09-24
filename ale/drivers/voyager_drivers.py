2abfd548f595db5da898bc056d82fdcaf9884c15
Kelvin <kelvinrr@icloud.com>
2019-09-24 10:40:22 -0700

    sick coding is bad coding

--- ale/drivers/voyager_drivers.py
+++ ale/drivers/voyager_drivers.py
@@ -31,7 +31,7 @@ class VoyagerCameraLabelNaifSpiceDriver(Framer, IsisLabel, NaifSpice, Driver):
     @property
     def spacecraft_name(self):
         name = super().spacecraft_name.replace('_', ' ')
-        if name.split(' ')[0] == "VOYAGER":
+        if name.split(' ')[0] != "VOYAGER":
             raise Exception("{name} for label is not a valid Voyager spacecraft name")
         return name
 
