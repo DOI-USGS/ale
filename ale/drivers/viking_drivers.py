f89aa4c38875dc8f442d6e70061ffcd26e3ae5c2
Peter Giroux <pgiroux@contractor.usgs.gov>
2019-09-30 12:54:39 -0700

    added load tests and data for vo2 cam a & b, vo1 cam a

--- ale/drivers/viking_drivers.py
+++ ale/drivers/viking_drivers.py
@@ -6,7 +6,14 @@ from ale.base.label_isis import IsisLabel
 from ale.base.type_sensor import Framer
 from ale.base.base import Driver
 
+ALT_ID_LOOKUP = {
+    'VIKING ORBITER 1': -27999,
+    'VIKING ORBITER 2':-30999
+}
+
 class VikingIsisLabelNaifSpiceDriver(Framer, IsisLabel, NaifSpice, Driver):
+
+
 
     @property
     def spacecraft_name(self):
@@ -19,6 +26,21 @@ class VikingIsisLabelNaifSpiceDriver(Framer, IsisLabel, NaifSpice, Driver):
           Name of the spacecraft.
         """
         return super().spacecraft_name.replace('_', ' ')
+
+    @property
+    def alt_ikid(self):
+        """
+        Viking Orbiter 1 & 2 each have an alternate naif id code as defined in the
+        SCLK kernels.
+        Expects spacecraft name to be defined.
+        Returns -27999 for vo1 and -30999 for vo2
+
+        Returns
+        -------
+        : integer
+        Alternate Naif Integer ID code for the instrument
+        """
+        return ALT_ID_LOOKUP[self.spacecraft_name]
 
     @property
     def ikid(self):
@@ -36,8 +58,8 @@ class VikingIsisLabelNaifSpiceDriver(Framer, IsisLabel, NaifSpice, Driver):
     @property
     def ephemeris_start_time(self):
         """
-        Overridden to use the alternate instrument ID '-27999' as defined in the
-        SCLK kernel. Also computes an offset to match what is being done in ISIS code.
+        Overridden to use the alternate instrument ID. Also computes an offset to match
+        what is being done in ISIS code.
         Expects spacecraft_clock_start_count to be defined.
 
         Returns
@@ -45,12 +67,7 @@ class VikingIsisLabelNaifSpiceDriver(Framer, IsisLabel, NaifSpice, Driver):
         : float
           ephemeris start time of the image
         """
-        if self.spacecraft_name == 'VIKING ORBITER 1':
-            ephemeris_start_time = spice.scs2e(-27999, str(self.spacecraft_clock_start_count))
-
-        # else use vo2 alt ID
-        else:
-            ephemeris_start_time = spice.scs2e(-30999, str(self.spacecraft_clock_start_count))
+        ephemeris_start_time = spice.scs2e(self.alt_ikid, str(self.spacecraft_clock_start_count))
 
         if self.exposure_duration <= .420:
             offset1 = 7.0 / 8.0 * 4.48
