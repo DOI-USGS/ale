824736855fcee4b0fd95b6f9e743c1c32eb0c6c6
Adam Paquette <acp263@nau.edu>
2019-09-30 12:12:01 -0700

    Updated starting ephemeris time based on isis implementation

--- ale/drivers/juno_drivers.py
+++ ale/drivers/juno_drivers.py
@@ -12,6 +12,21 @@ class JunoJunoCamIsisLabelNaifSpiceDriver(Framer, IsisLabel, NaifSpice, Driver):
         return look_up[super().instrument_id]
 
     @property
+    def ephemeris_start_time(self):
+        if not hasattr(self, '_ephemeris_start_time'):
+            # Junos camera is split into stacked frames where an image is made
+            # of sets of RGBM chuncks. We need to account for these chuncks since
+            # ISIS produces some number of cubes N where N = M*4.
+            # Computation obtained from JunoCamera.cpp
+            initial_time = super().ephemeris_start_time
+            frame_number = self.label['IsisCube']['Instrument']['FrameNumber']
+            inter_frame_delay = self.label['IsisCube']['Instrument']['InterFrameDelay'].value
+            start_time_bias = self.naif_keywords[f'INS{self.ikid}_START_TIME_BIAS']
+            inter_frame_delay_bias = self.naif_keywords[f'INS{self.ikid}_INTERFRAME_DELTA']
+            self._ephemeris_start_time = initial_time + start_time_bias + (frame_number - 1) * (inter_frame_delay + inter_frame_delay_bias)
+        return self._ephemeris_start_time
+
+    @property
     def sensor_model_version(self):
         return 1
 
