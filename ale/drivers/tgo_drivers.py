116e05b74b66da74bd1c34a6876cab334f931748
paarongiroux <47163875+paarongiroux@users.noreply.github.com>
2019-10-07 15:36:35 -0700

    TGO Cassis ALE driver and light time to surface correction (#295)
    * Adds TGO Cassis ALE driver and adds light time to surface correction
    
    * Add test data
    
    * updated cassis and kaguya tests to get them running

---
+++ ale/drivers/tgo_drivers.py
@@ -0,0 +1,66 @@
+from glob import glob
+import os
+
+import struct
+import pvl
+import spiceypy as spice
+import numpy as np
+
+from ale.base import Driver
+from ale.base.data_naif import NaifSpice
+from ale.base.label_isis import IsisLabel
+from ale.base.type_sensor import Framer
+
+class TGOCassisIsisLabelNaifSpiceDriver(Framer, IsisLabel, NaifSpice, Driver):
+    """
+    Driver for reading TGO Cassis ISIS3 Labels. These are Labels that have been ingested
+    into ISIS from PDS EDR images but have not been spiceinit'd yet.
+    """
+    @property
+    def instrument_id(self):
+        """
+        Returns an instrument id for unquely identifying the instrument, but often
+        also used to be piped into Spice Kernels to acquire IKIDs. Therefore they
+        the same ID the Spice expects in bods2c calls.
+        Expects instrument_id to be defined in the Pds3Label mixin. This should
+        be a string of the form CaSSIS
+
+        Returns
+        -------
+        : str
+          instrument id
+        """
+        id_lookup = {
+            'CaSSIS': 'TGO_CASSIS',
+        }
+        return id_lookup[super().instrument_id]
+
+    @property
+    def ephemeris_start_time(self):
+        """
+        Returns the ephemeris_start_time of the image.
+        Expects spacecraft_clock_start_count to be defined. This should be a float
+        containing the start clock count of the spacecraft.
+        Expects spacecraft_id to be defined. This should be the integer Naif ID code
+        for the spacecraft.
+
+        Returns
+        -------
+        : float
+          ephemeris start time of the image.
+        """
+        return spice.utc2et(str(self.label['IsisCube']['Instrument']['StartTime']))
+
+    @property
+    def sensor_frame_id(self):
+        return -143420
+
+    @property
+    def sensor_model_version(self):
+        """
+        Returns
+        -------
+        : int
+          ISIS sensor model version
+        """
+        return 1
