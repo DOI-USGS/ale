755694681f2985345fa000328843b29a218f1735
Peter Giroux <pgiroux@contractor.usgs.gov>
2019-10-11 12:10:14 -0700

    unfinished lronac isis driver work

--- ale/drivers/lro_drivers.py
+++ ale/drivers/lro_drivers.py
@@ -8,6 +8,7 @@ from ale.util import get_metakernels
 from ale.base import Driver
 from ale.base.data_naif import NaifSpice
 from ale.base.label_pds3 import Pds3Label
+from ale.base.label_isis import IsisLabel
 from ale.base.type_sensor import LineScanner
 
 
@@ -220,3 +221,202 @@ class LroLrocPds3LabelNaifSpiceDriver(LineScanner, NaifSpice, Pds3Label, Driver)
           Number of samples and lines combined from the original data to produce a single pixel in this image
         """
         return self.crosstrack_summing
+
+class LroLrocIsisLabelNaifSpiceDriver(LineScanner, NaifSpice, IsisLabel, Driver):
+    @property
+    def instrument_id(self):
+        """
+        The short text name for the instrument
+
+        Returns an instrument id uniquely identifying the instrument. Used to acquire
+        instrument codes from Spice Lib bods2c routine.
+
+        Returns
+        -------
+        str
+          The short text name for the instrument
+        """
+        id_lookup = {
+            "NACL": "LRO_LROCNACL",
+            "NACR": "LRO_LROCNACR"
+        }
+
+        return id_lookup[super().instrument_id]
+
+    @property
+    def sensor_model_version(self):
+        """
+        Returns ISIS instrument sensor model version number
+
+        Returns
+        -------
+        : int
+          ISIS sensor model version
+        """
+        return 2
+
+    @property
+    def usgscsm_distortion_model(self):
+        """
+        The distortion model name with its coefficients
+
+        LRO LROC NAC does not use the default distortion model so we need to overwrite the
+        method packing the distortion model into the ISD.
+
+        Returns
+        -------
+        : dict
+          Returns a dict with the model name : dict of the coefficients
+        """
+
+        return {"lrolrocnac":
+                {"coefficients": self.odtk}}
+
+    @property
+    def odtk(self):
+        """
+        The coefficients for the distortion model
+
+        Returns
+        -------
+        : list
+          Radial distortion coefficients. There is only one coefficient for LROC NAC l/r
+        """
+        return spice.gdpool('INS{}_OD_K'.format(self.ikid), 0, 1).tolist()
+
+    @property
+    def light_time_correction(self):
+        """
+        Returns the type of light time correciton and abberation correction to
+        use in NAIF calls.
+
+        LROC is specifically set to not use light time correction because it is
+        so close to the surface of the moon that light time correction to the
+        center of the body is incorrect.
+
+        Returns
+        -------
+        : str
+          The light time and abberation correction string for use in NAIF calls.
+          See https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/req/abcorr.html
+          for the different options available.
+        """
+        return 'NONE'
+
+    @property
+    def detector_center_sample(self):
+        """
+        The center of the CCD in detector pixels
+        ISIS uses 0.5 based CCD samples, so we need to convert to 0 based.
+
+        Returns
+        -------
+        float :
+            The center sample of the CCD
+        """
+        return super().detector_center_sample - 0.5
+
+    @property
+    def ephemeris_start_time(self):
+        """
+        The starting ephemeris time for LRO is computed by taking the
+        LRO:SPACECRAFT_CLOCK_PREROLL_COUNT, as defined in the label, and
+        adding offsets that were taken from an IAK.
+        -------
+        : double
+          Starting ephemeris time of the image
+        """
+        start_time = spice.scs2e(self.spacecraft_id, self.label['IsisCube']['Instrument']['SpacecraftClockPrerollCount'])
+        return start_time + self.constant_time_offset + self.additional_preroll * self.exposure_duration
+
+    @property
+    def exposure_duration(self):
+        """
+        Takes the exposure_duration defined in a parent class and adds
+        offsets taken from an IAK.
+
+         Returns
+         -------
+         : float
+           Returns the exposure duration in seconds.
+         """
+        return super().exposure_duration * (1 + self.multiplicative_line_error) + self.additive_line_error
+
+    @property
+    def multiplicative_line_error(self):
+        """
+        Returns the multiplicative line error defined in an IAK.
+
+         Returns
+         -------
+         : float
+           Returns the multiplicative line error.
+         """
+        return 0.0045
+
+    @property
+    def additive_line_error(self):
+        """
+        Returns the additive line error defined in an IAK.
+
+         Returns
+         -------
+         : float
+           Returns the additive line error.
+         """
+        return 0.0
+
+    @property
+    def constant_time_offset(self):
+        """
+        Returns the constant time offset defined in an IAK.
+
+         Returns
+         -------
+         : float
+           Returns the constant time offset.
+         """
+        return 0.0
+
+    @property
+    def additional_preroll(self):
+        """
+        Returns the addition preroll defined in an IAK.
+
+         Returns
+         -------
+         : float
+           Returns the additionl preroll.
+         """
+        return 1024.0
+
+    @property
+    def sampling_factor(self):
+        """
+        Returns the summing factor from the PDS3 label that is defined by the CROSSTRACK_SUMMING.
+        For example a return value of 2 indicates that 2 lines and 2 samples (4 pixels)
+        were summed and divided by 4 to produce the output pixel value.
+
+        Returns
+        -------
+        : int
+          Number of samples and lines combined from the original data to produce a single pixel in this image
+        """
+        return self.label['IsisCube']['Instrument']['SpatialSumming']
+
+    @property
+    def target_frame_id(self):
+        """
+        Returns the Naif ID code for the target body
+        Expects target_name to be defined. This must be a string containig the name
+        of the target body.
+
+        Returns
+        -------
+        : int
+          Naif ID code for the target body
+        """
+        name_lookup = {
+            "MOON": "MOON_ME"
+        }
+        return spice.gdpool('FRAME_{}'.format(name_lookup[self.target_name]),0,1)
--- tests/pytests/test_lro_drivers.py
+++ tests/pytests/test_lro_drivers.py
@@ -1,22 +1,138 @@
 import pytest
 import numpy as np
 import os
+import unittest
 from unittest.mock import PropertyMock, patch
+import spiceypy as spice
+import json
 
 import ale
 from ale import util
-from ale.drivers import lro_drivers
+# from ale.drivers import lro_drivers
 from ale.drivers.lro_drivers import LroLrocPds3LabelNaifSpiceDriver
+from ale.drivers.lro_drivers import LroLrocIsisLabelNaifSpiceDriver
 
 from conftest import get_image_label, get_image_kernels, convert_kernels, compare_dicts
 
-@pytest.fixture()
+image_dict = {
+    'M103595705LE': {
+    'isis': {
+        'CameraVersion': 2,
+        'NaifKeywords': {'BODY_CODE': 301,
+                         'BODY301_RADII': [1737.4, 1737.4, 1737.4],
+                         'BODY_FRAME_CODE': 31001,
+                         'INS-85600_SWAP_OBSERVER_TARGET': 'TRUE',
+                         'INS-85600_LIGHTTIME_CORRECTION': 'NONE',
+                         'INS-85600_LT_SURFACE_CORRECT': 'TRUE',
+                         'INS-85600_FOCAL_LENGTH': 699.62,
+                         'INS-85600_PIXEL_PITCH': 0.007,
+                         'INS-85600_CONSTANT_TIME_OFFSET': 0.0,
+                         'INS-85600_ADDITIONAL_PREROLL': 1024.0,
+                         'INS-85600_ADDITIVE_LINE_ERROR': 0.0,
+                         'INS-85600_MULTIPLI_LINE_ERROR': 0.0045,
+                         'INS-85600_TRANSX': [0.0, 0.0, 0.007],
+                         'INS-85600_TRANSY': [0.0, 0.007, 0.0],
+                         'INS-85600_ITRANSS': [0.0, 0.0, 142.857],
+                         'INS-85600_ITRANSL': [0.0, 142.857, 0.0],
+                         'INS-85600_BORESIGHT_SAMPLE': 2548.0,
+                         'INS-85600_BORESIGHT_LINE': 0.0,
+                         'INS-85600_OD_K': 1.81e-05,
+                         # begin extras
+                         'INS-85600_CK_FRAME_ID': -85000.0,
+                         'TKFRAME_-85600_RELATIVE': 'LRO_SC_BUS',
+                         'INS-85600_PIXEL_SAMPLES': 5064.0,
+                         'INS-85600_WAVELENGTH_RANGE': [400., 760.],
+                         'INS-85600_FOV_BOUNDARY_CORNERS':([ 4.9738e-06, -2.5336e-02,  1.0000e+00,
+                                                             4.9747e-06, -2.4943e-02, 1.0000e+00,
+                                                             5.0027e-06,  0.0000e+00,  1.0000e+00,  4.9750e-06]),
+                         'FRAME_-85600_NAME': 'LRO_LROCNACL',
+                         'CK_-85600_SPK': -85.0,
+                         'CK_-85600_SCLK': -85.0,
+                         'INS-85600_PIXEL_LINES': 1.0,
+                         'INS-85600_BORESIGHT': [0., 0., 1.],
+                         'INS-85600_PLATFORM_ID': -85000.0,
+                         'FRAME_-85600_CLASS': 3.0,
+                         'INS-85600_F/RATIO': 3.577,
+                         'INS-85600_FOV_SHAPE': 'POLYGON',
+                         'INS-85600_PIXEL_SIZE': [0.007, 0.007],
+                         'INS-85600_CK_REFERENCE_ID': 1.0,
+                         'INS-85600_FOV_FRAME': 'LRO_LROCNACL',
+                         'FRAME_-85600_CLASS_ID': -85600.0,
+                         'INS-85600_IFOV': 1.0005399999999999e-05,
+                         'FRAME_-85600_CENTER': -85.0,
+                         'INS-85600_CCD_CENTER':[2.5325e+03, 1.0000e+00],
+                         'BODY301_POLE_RA': [2.699949e+02, 3.100000e-03, 0.000000e+00],
+                         'BODY301_NUT_PREC_PM': [ 3.561e+00,  1.208e-01, -6.420e-02,  1.580e-02,  2.520e-02,
+                                                 -6.600e-03, -4.700e-03, -4.600e-03,  2.800e-03,  5.200e-03],
+                         'BODY301_NUT_PREC_RA': [-3.8787, -0.1204,  0.07  , -0.0172,  0.    ,  0.0072,  0.    ,
+                                                 0.    ,  0.    , -0.0052],
+                         'BODY301_LONG_AXIS': 0.0,
+                         'BODY301_NUT_PREC_DEC': [ 1.5419e+00,  2.3900e-02, -2.7800e-02,  6.8000e-03,  0.0000e+00,
+                                                  -2.9000e-03,  9.0000e-04,  0.0000e+00,  0.0000e+00,  8.0000e-04],
+                         'BODY301_POLE_DEC': [6.65392e+01, 1.30000e-02, 0.00000e+00],
+                         'BODY301_PM': [ 3.83213000e+01,  1.31763582e+01, -1.40000000e-12]
+                         },
+        'InstrumentPointing': {'TimeDependentFrames': [-85600, -85000, 1],
+                               'CkTableStartTime': 302228504.36825,
+                               'CkTableEndTime': 302228558.33808,
+                               'CkTableOriginalSize': 6,
+                               'EphemerisTimes': [302228504.36825, 302228504.45712, 302228504.55737,
+                                                  302228504.65658, 302228504.75889, 302228504.85913],
+                               'Quaternions':[[0.22984449090659,0.85623601215262,-0.014576324183122,0.46240559281456],
+                                              [0.22984460552989,0.85621850640729,-0.01458386095068,0.46243771212376],
+                                              [0.22984338971027,0.85619855565584,-0.01459216867542,0.46247499187814],
+                                              [0.22984159131414,0.85617947685547,-0.014601074862042,0.46251092411591],
+                                              [0.22984103471996,0.85615941772671,-0.014609570264943,0.46254806307387],
+                                              [0.22984006962974,0.85613990798999,-0.014618107324828,0.46258438287271]],
+                               'AngularVelocity': [[1.57962886767719e-04,-7.64378257059964e-04,-3.17453142822095e-04],
+                                                   [1.36538192734999e-04,-7.82531384781112e-04,-3.29868347602467e-04],
+                                                   [1.29895333117494e-04,-7.56320525422279e-04,-3.42502494121226e-04],
+                                                   [1.47073256621385e-04,-7.6466249251597e-04,-3.197497534627e-04],
+                                                   [1.41188264842957e-04,-7.61008664016974e-04,-3.26945565632267e-04],
+                                                   [1.37079967629511e-04,-7.66620237251365e-04,-3.38019650057636e-04]]},
+        'BodyRotation': {'TimeDependentFrames': [31006, 1],
+                         'ConstantFrames': [31001, 31007, 31006],
+                         'ConstantRotation': [0.99999987325471, -3.29285422375571e-04,
+                                              3.80869618671387e-04, 3.29286000210947e-04,
+                                              0.99999994578431, -1.45444093783627e-06,
+                                              -3.80869119096078e-04, 1.57985578682691e-06,
+                                              0.99999992746811],
+                         'CkTableStartTime': 302228504.36825,
+                         'CkTableEndTime': 302228558.33808,
+                         'CkTableOriginalSize': 6,
+                         'EphemerisTimes': [302228504.36825, 302228558.33808],
+                         'Quaternions': [[0.88962945264394,-0.18337484217069,0.071507844537859,-0.4120918980238],
+                                         [0.88959985815153,-0.18336967062883,0.071521012825659,-0.41215579699264]],
+                         'AngularVelocity': [[6.23828510009554e-08,-1.02574900146521e-06,2.4553540362001e-06],
+                                             [6.23829070888373e-08,-1.02574904903342e-06,2.4553540436861e-06]]},
+        'InstrumentPosition': {'SpkTableStartTime': 302228504.36825,
+                               'SpkTableEndTime': 302228558.33808,
+                               'SpkTableOriginalSize': 52225.0,
+                               'EphemerisTimes': [],
+                               'Positions': [[]],
+                               'Velocities': [[]]},
+        'SunPosition': {'SpkTableStartTime': 302228504.36825,
+                        'SpkTableEndTime': 302228558.33808,
+                        'SpkTableOriginalSize': 2.0,
+                        'EphemerisTimes': [302228504.36825, 302228558.33808],
+                        'Positions': [[-91885591.670166,111066642.35741,48186232.184905],
+                                      [-91886885.638289,111065783.10638,48185857.627349]],
+                        'Velocities': [[-23.97582468998,-15.92078929651,-6.9400521199436],
+                                       [-23.975713622197,-15.921114625634,-6.9402056052972]]}
+    }}
+}
+
+@pytest.fixture(scope="module")
 def test_kernels():
-    kernels = get_image_kernels('M103595705LE')
-    updated_kernels, binary_kernels = convert_kernels(kernels)
+    updated_kernels = {}
+    binary_kernels = {}
+    for image in image_dict.keys():
+        kernels = get_image_kernels(image)
+        updated_kernels[image], binary_kernels[image] = convert_kernels(kernels)
     yield updated_kernels
-    for kern in binary_kernels:
-        os.remove(kern)
+    for kern_list in binary_kernels.values():
+        for kern in kern_list:
+            os.remove(kern)
 
 @pytest.fixture(params=["Pds3NaifDriver"])
 def driver(request):
@@ -135,8 +251,85 @@ def test_exposure_duration(driver):
         exposure_duration.return_value = 1
         assert driver.exposure_duration == 1.0045
 
-def test_load(test_kernels, usgscsm_comparison_isd):
-    label_file = get_image_label('M103595705LE')
-    usgscsm_isd = ale.load(label_file, props={'kernels': test_kernels}, formatter='usgscsm')
-    print(usgscsm_isd)
-    assert compare_dicts(usgscsm_isd, usgscsm_comparison_isd) == []
+# usgscsm isd
+# def test_load_usgs(test_kernels, usgscsm_comparison_isd):
+#     label_file = get_image_label('M103595705LE', 'pds3')
+#     usgscsm_isd = ale.load(label_file, props={'kernels': test_kernels}, formatter='usgscsm')
+#     print(usgscsm_isd)
+#     assert compare_dicts(usgscsm_isd, usgscsm_comparison_isd) == []
+
+@pytest.mark.parametrize("label_type", ['isis3'])
+@pytest.mark.parametrize("formatter", ['isis'])
+@pytest.mark.parametrize("image", image_dict.keys())
+def test_load_isis(test_kernels, label_type, formatter, image):
+    label_file = get_image_label(image, label_type)
+    isis_isd = ale.loads(label_file, props={'kernels': test_kernels[image]}, formatter=formatter, verbose=True)
+    isis_isd_obj = json.loads(isis_isd)
+    print(json.dumps(isis_isd_obj, indent=4))
+    assert compare_dicts(isis_isd_obj, image_dict[image][formatter]) == []
+
+# ========= Test isislabel and naifspice driver =========
+class test_isis_naif(unittest.TestCase):
+
+    def setUp(self):
+        label = get_image_label('M103595705LE', 'isis3')
+        self.driver = LroLrocIsisLabelNaifSpiceDriver(label)
+
+    def test_short_mission_name(self):
+        assert self.driver.short_mission_name == 'lro'
+
+    def test_intrument_id(self):
+        assert self.driver.instrument_id == 'LRO_LROCNACL'
+
+    def test_usgscsm_distortion_model(self):
+        with patch('ale.drivers.lro_drivers.spice.gdpool', return_value=np.array([1.0])) as gdpool, \
+             patch('ale.drivers.lro_drivers.spice.bods2c', return_value=-12345) as bods2c:
+            distortion_model = self.driver.usgscsm_distortion_model
+            assert distortion_model['lrolrocnac']['coefficients'] == [1.0]
+            gdpool.assert_called_with('INS-12345_OD_K', 0, 1)
+            bods2c.assert_called_with('LRO_LROCNACL')
+
+    def test_odtk(self):
+        with patch('ale.drivers.lro_drivers.spice.gdpool', return_value=np.array([1.0])) as gdpool, \
+             patch('ale.drivers.lro_drivers.spice.bods2c', return_value=-12345) as bods2c:
+             assert self.driver.odtk == [1.0]
+             gdpool.assert_called_with('INS-12345_OD_K', 0, 1)
+             bods2c.assert_called_with('LRO_LROCNACL')
+
+    def test_light_time_correction(self):
+        assert self.driver.light_time_correction == 'NONE'
+
+    def test_detector_center_sample(self):
+        with patch('ale.drivers.lro_drivers.spice.gdpool', return_value=np.array([1.0])) as gdpool, \
+             patch('ale.drivers.lro_drivers.spice.bods2c', return_value=-12345) as bods2c:
+            assert self.driver.detector_center_sample == 0.5
+            gdpool.assert_called_with('INS-12345_BORESIGHT_SAMPLE', 0, 1)
+            bods2c.assert_called_with('LRO_LROCNACL')
+
+    def test_exposure_duration(self):
+        np.testing.assert_almost_equal(self.driver.exposure_duration, .0010334296)
+
+    def test_ephemeris_start_time(self):
+        with patch('ale.drivers.lro_drivers.spice.scs2e', return_value=321) as scs2e:
+            np.testing.assert_almost_equal(self.driver.ephemeris_start_time, 322.05823191)
+            scs2e.assert_called_with(-85, '1/270649237:07208')
+
+    def test_multiplicative_line_error(self):
+        assert self.driver.multiplicative_line_error == 0.0045
+
+    def test_additive_line_error(self):
+        assert self.driver.additive_line_error == 0
+
+    def test_constant_time_offset(self):
+        assert self.driver.constant_time_offset == 0
+
+    def test_additional_preroll(self):
+        assert self.driver.additional_preroll == 1024
+
+    def test_sampling_factor(self):
+        assert self.driver.sampling_factor == 1
+
+    def test_target_frame_id(self):
+        with patch('ale.drivers.lro_drivers.spice.gdpool', return_value=-12345) as gdpool:
+            assert self.driver.target_frame_id == -12345
+            gdpool.assert_called_with('FRAME_MOON_ME',0,1)
