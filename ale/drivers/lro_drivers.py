5072f54ceaa2f99fa3894ba43ca714470e69448f
Kelvin <kelvinrr@icloud.com>
2019-10-15 01:19:40 -0700

    patched int line

--- ale/drivers/lro_drivers.py
+++ ale/drivers/lro_drivers.py
@@ -419,4 +419,4 @@ class LroLrocIsisLabelNaifSpiceDriver(LineScanner, NaifSpice, IsisLabel, Driver)
         name_lookup = {
             "MOON": "MOON_ME"
         }
-        return int(spice.gdpool('FRAME_{}'.format(name_lookup[self.target_name]),0,1)[0])
+        return int(spice.gdpool('FRAME_{}'.format(name_lookup[self.target_name]),0,1))
