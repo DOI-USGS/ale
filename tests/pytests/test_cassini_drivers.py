358cec074407e08b6346c4a3355356e2519b435b
Kelvin <kelvinrr@icloud.com>
2020-04-01 12:28:41 -0700

    Updated Cassini ISS tests

--- tests/pytests/test_cassini_drivers.py
+++ tests/pytests/test_cassini_drivers.py
@@ -6,49 +6,11 @@ import numpy as np
 from ale.drivers import co_drivers
 import unittest
 from unittest.mock import patch
-
-from conftest import get_image_label, get_image_kernels, convert_kernels, compare_dicts
+import json
+from conftest import get_image_label, get_image_kernels, get_isd, convert_kernels, compare_dicts
 
 from ale.drivers.co_drivers import CassiniIssPds3LabelNaifSpiceDriver
 from conftest import get_image_kernels, convert_kernels, get_image_label
-
-@pytest.fixture()
-def usgscsm_compare_dict():
-    return {'radii':
-             {'semimajor': 256.6,
-               'semiminor': 248.3,
-               'unit': 'km'},
-            'sensor_position':
-              {'positions': np.array([[-2256845.0758612, -24304022.50656576, -639184.92761627]]),
-               'velocities': np.array([[-6889.88091886, -2938.52964751, 21.84486935]]),
-               'unit': 'm'},
-            'sun_position':
-              {'positions': [np.array([-2.30750862e+11, -1.39662303e+12, 3.09841240e+11])],
-               'velocities': [np.array([-7.41107317e+07, 1.22577658e+07, 3.91731010e+03])],
-               'unit': 'm'},
-            'sensor_orientation':
-              {'quaternions': np.array([[-0.21183452, -0.66567547, -0.69966839,  0.14988809]])},
-            'detector_sample_summing': 1,
-            'detector_line_summing': 1,
-            'focal_length_model': {'focal_length': 2003.09},
-            'detector_center': {'line': 512, 'sample': 512},
-            'starting_detector_line': 0,
-            'starting_detector_sample': 0,
-            'focal2pixel_lines': [0.0, 0.0, 83.33333333333333],
-            'focal2pixel_samples': [0.0, 83.33333333333333, 0.0],
-            'optical_distortion':
-              {'radial':
-                {'coefficients': [0, -8e-06, 0]}},
-            'image_lines': 1024,
-            'image_samples': 1024,
-            'name_platform': 'CASSINI ORBITER',
-            'name_sensor': 'IMAGING SCIENCE SUBSYSTEM - NARROW ANGLE',
-            'reference_height':
-              {'maxheight': 1000,
-               'minheight': -1000,
-               'unit': 'm'},
-            'name_model': 'USGS_ASTRO_FRAME_SENSOR_MODEL',
-            'center_ephemeris_time': 376938208.24072826}
 
 @pytest.fixture()
 def test_kernels(scope="module", autouse=True):
@@ -58,11 +20,14 @@ def test_kernels(scope="module", autouse=True):
     for kern in binary_kernels:
         os.remove(kern)
 
-def test_cassini_load(test_kernels, usgscsm_compare_dict):
+def test_load(test_kernels):
     label_file = get_image_label("N1702360370_1")
-    usgscsm_isd = ale.load(label_file, props={'kernels': test_kernels}, formatter='usgscsm')
-    print(usgscsm_isd)
-    assert compare_dicts(usgscsm_isd, usgscsm_compare_dict) == []
+    compare_dict = get_isd("cassiniiss")
+
+    isd_str = ale.loads(label_file, props={'kernels': test_kernels})
+    isd_obj = json.loads(isd_str)
+    print(json.dumps(isd_obj, indent=2))
+    assert compare_dicts(isd_obj, compare_dict) == []
 
 # ========= Test cassini pds3label and naifspice driver =========
 class test_cassini_pds3_naif(unittest.TestCase):
