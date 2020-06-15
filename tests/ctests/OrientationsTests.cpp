c39e6c46b4bdb0aa6e40cebd406f885b7a65fdb6
Jesse Mapel <jmapel@usgs.gov>
2020-06-15 14:55:58 -0700

    Added Orientation inverse test

--- tests/ctests/OrientationsTests.cpp
+++ tests/ctests/OrientationsTests.cpp
@@ -34,7 +34,9 @@ class ConstOrientationTest : public OrientationTest{
     void SetUp() override {
       OrientationTest::SetUp();
       constRotation = Rotation(0, 1, 0, 0);
-      constOrientations = Orientations(rotations, times, avs, constRotation);
+      vector<int> constFrames = {-74021, -74020, -74000};
+      vector<int> timeDepFrames = {-74000, -74900, 1};
+      constOrientations = Orientations(rotations, times, avs, constRotation, constFrames, timeDepFrames);
     }
 
     Rotation constRotation;
@@ -181,4 +183,58 @@ TEST_F(ConstOrientationTest, OrientationMultiplication) {
   EXPECT_EQ(constQuats[1], 1);
   EXPECT_EQ(constQuats[2], 0);
   EXPECT_EQ(constQuats[3], 0);
+}
+
+TEST_F(ConstOrientationTest, OrientationInverse) {
+  Orientations inverseOrientation = constOrientations.inverse();
+
+  vector<int> constFrames = inverseOrientation.getConstantFrames();
+  ASSERT_EQ(constFrames.size(), 0);
+
+  vector<int> timeDepFrames = inverseOrientation.getTimeDependentFrames();
+  ASSERT_EQ(timeDepFrames.size(), 5);
+  EXPECT_EQ(timeDepFrames[0], 1);
+  EXPECT_EQ(timeDepFrames[1], -74900);
+  EXPECT_EQ(timeDepFrames[2], -74000);
+  EXPECT_EQ(timeDepFrames[3], -74020);
+  EXPECT_EQ(timeDepFrames[4], -74021);
+
+  vector<double> newTimes = inverseOrientation.getTimes();
+  ASSERT_EQ(newTimes.size(), 3);
+  EXPECT_EQ(newTimes[0], 0.0);
+  EXPECT_EQ(newTimes[1], 2.0);
+  EXPECT_EQ(newTimes[2], 4.0);
+
+  vector<Rotation> newRotations = inverseOrientation.getRotations();
+  vector<vector<double>> expectedQuats = {
+    {-0.5, -0.5, 0.5, -0.5},
+    {-0.5, 0.5, 0.5, -0.5},
+    {0.0, -1.0, 0.0, 0.0}
+  };
+  ASSERT_EQ(newRotations.size(), expectedQuats.size());
+  for (size_t i = 0; i < newRotations.size(); i++) {
+    vector<double> newQuats = newRotations[i].toQuaternion();
+    ASSERT_EQ(newQuats.size(), 4) << "Rotation " << i;
+    EXPECT_EQ(newQuats[0], expectedQuats[i][0]) << "Rotation " << i;
+    EXPECT_EQ(newQuats[1], expectedQuats[i][1]) << "Rotation " << i;
+    EXPECT_EQ(newQuats[2], expectedQuats[i][2]) << "Rotation " << i;
+    EXPECT_EQ(newQuats[3], expectedQuats[i][3]) << "Rotation " << i;
+  }
+
+  vector<Vec3d> newAvs = inverseOrientation.getAngularVelocities();
+  vector<Vec3d> expectedAvs = {
+    Vec3d(-2.0 / 3.0 * M_PI, 2.0 / 3.0 * M_PI, 2.0 / 3.0 * M_PI),
+    Vec3d(-2.0 / 3.0 * M_PI, 2.0 / 3.0 * M_PI, 2.0 / 3.0 * M_PI),
+    Vec3d(-2.0 / 3.0 * M_PI, 2.0 / 3.0 * M_PI, 2.0 / 3.0 * M_PI)
+  };
+  ASSERT_EQ(newAvs.size(), expectedAvs.size());
+  EXPECT_EQ(newAvs[0].x, expectedAvs[0].x);
+  EXPECT_EQ(newAvs[0].y, expectedAvs[0].y);
+  EXPECT_EQ(newAvs[0].z, expectedAvs[0].z);
+  EXPECT_EQ(newAvs[1].x, expectedAvs[1].x);
+  EXPECT_EQ(newAvs[1].y, expectedAvs[1].y);
+  EXPECT_EQ(newAvs[1].z, expectedAvs[1].z);
+  EXPECT_EQ(newAvs[2].x, expectedAvs[2].x);
+  EXPECT_EQ(newAvs[2].y, expectedAvs[2].y);
+  EXPECT_EQ(newAvs[2].z, expectedAvs[2].z);
 }
