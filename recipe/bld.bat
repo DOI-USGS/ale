1bffc52ed0b90e81681bb1187ee00c21e67c9a75
Jesse Mapel <jmapel@usgs.gov>
2020-06-22 11:18:49 -0700

    Testing adding back python

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,19 @@
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON -DALE_BUILD_TESTS=OFF -G "Visual Studio 15 2017 Win64" ..
-cmake --build . --target ALL_BUILD --config Release
+
+cmake   -GNinja ^
+        -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON ^
+        -DALE_BUILD_TESTS=OFF ^
+        -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
+        -DCMAKE_INSTALL_LIBDIR="%LIBRARY_LIB%" ^
+        -DCMAKE_INSTALL_INCLUDEDIR="%LIBRARY_INC%" ^
+        -DCMAKE_INSTALL_BINDIR="%LIBRARY_BIN%" ^
+        -DCMAKE_INSTALL_DATADIR="%LIBRARY_PREFIX%" ^
+        ..
+
+cmake --build . --target install --config Release
+
+cd ..
+pip install . --no-deps -vv
 
 if errorlevel 1 exit 1
