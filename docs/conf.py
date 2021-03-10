59569c6bb2b6947b88e519f09f939a0bbdad8492
ladoramkershner <ladoramkershner@gmail.com>
2021-03-10 15:06:20 -0700

    typo

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ extensions = [
 templates_path = ['_templates']
 
 # The master toctree document.
-master_doc = 'index.rst'
+master_doc = 'index'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
