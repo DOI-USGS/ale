6bb6787f544713685a3967a3eee7072d2ce1c604
Jesse Mapel <jmapel@usgs.gov>
2021-03-16 13:56:32 -0700

    Renamed doxyfile

--- docs/conf.py
+++ docs/conf.py
@@ -62,4 +62,4 @@ import subprocess, os
 read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 if read_the_docs_build:
-    subprocess.call('cd ../doxygen; doxygen', shell=True)
+    subprocess.call('doxygen', shell=True)
