2856500286a51eab0b0039030e0f981a1afc3008
AustinSanders <austinsanders1993@gmail.com>
2022-03-09 13:13:33 -0700

    Initial KaguyaMiIsisLabelNaifSpiceDriver (#435)
    * Initial KaguyaMiIsisLabelNaifSpiceDriver
    
    * Initial kaguyamiisislabelnaifspicedriver and tests
    
    * kaguyami test data
    
    * Removed comment and conf files from data area
    
    * Updated comments to propertly reflect MI instrument

--- ale/drivers/selene_drivers.py
+++ ale/drivers/selene_drivers.py
@@ -6,6 +6,7 @@ import spiceypy as spice
 from ale.base import Driver
 from ale.base.data_naif import NaifSpice
 from ale.base.label_pds3 import Pds3Label
+from ale.base.label_isis import IsisLabel
 from ale.base.type_sensor import LineScanner
 
 
@@ -558,6 +559,7 @@ class KaguyaMiPds3NaifSpiceDriver(LineScanner, Pds3Label, NaifSpice, Driver):
         base_band = band_map[self.label.get("BASE_BAND")]
         return base_band
 
+
     @property
     def instrument_id(self):
         """
@@ -850,3 +852,333 @@ class KaguyaMiPds3NaifSpiceDriver(LineScanner, Pds3Label, NaifSpice, Driver):
           ISIS sensor model version
         """
         return 1
+
+
+class KaguyaMiIsisLabelNaifSpiceDriver(LineScanner, NaifSpice, IsisLabel, Driver):
+    @property
+    def base_band(self):
+        """
+        Which band the bands are registered to.
+
+        Returns
+        -------
+        base_band : str
+            The base band of the instrument
+        """
+        band_map = {
+            "MV1" : "MI-VIS1",
+            "MV2" : "MI-VIS2",
+            "MV3" : "MI-VIS3",
+            "MV4" : "MI-VIS4",
+            "MV5" : "MI-VIS5",
+            "MN1" : "MI-NIR1",
+            "MN2" : "MI-NIR2",
+            "MN3" : "MI-NIR3",
+            "MN4" : "MI-NIR4"
+        }
+        base_band = band_map[self.label['IsisCube']['BandBin']['BaseBand']]
+        return base_band
+
+    @property
+    def instrument_id(self):
+        """
+        Id takes the form of LISM_<BASE_BAND> where <BASE_BAND> is which band
+        the bands were registered to.
+
+        Returns
+        -------
+        : str
+          instrument id
+        """
+
+        id = f"LISM_{self.base_band}"
+        return id
+
+    @property
+    def sensor_model_version(self):
+        """
+        Returns ISIS instrument sensor model version number
+
+        Returns
+        -------
+        : int
+          ISIS sensor model version
+        """
+        return 2
+
+
+    @property
+    def spacecraft_clock_start_count(self):
+        """
+        The original SC_CLOCK_START_COUNT key is often incorrect and cannot be trusted.
+        Therefore we get this information from CORRECTED_SC_CLOCK_START_COUNT
+
+        Returns
+        -------
+        : float
+          spacecraft clock start count in seconds
+        """
+        return self.label['IsisCube']['Instrument']['CorrectedScClockStartCount'].value
+
+
+
+    @property
+    def spacecraft_clock_stop_count(self):
+        """
+        The original SC_CLOCK_START_COUNT key is often incorrect and cannot be trusted.
+        Therefore we get this information from CORRECTED_SC_CLOCK_STOP_COUNT
+
+        Returns
+        -------
+        : float
+          spacecraft clock start count in seconds
+        """
+        return self.label['IsisCube']['Instrument']['CorrectedScClockStopCount'].value
+
+
+    @property
+    def ephemeris_start_time(self):
+        """
+        Returns the ephemeris start time of the image. Expects spacecraft_id to
+        be defined. This should be the integer naif ID code of the spacecraft.
+
+        Returns
+        -------
+        : float
+          ephemeris start time of the image
+        """
+        return spice.sct2e(self.spacecraft_id, self.spacecraft_clock_start_count)
+
+
+    @property
+    def ephemeris_stop_time(self):
+        """
+        Returns the ephemeris start time of the image. Expects spacecraft_id to
+        be defined. This should be the integer naif ID code of the spacecraft.
+
+        Returns
+        -------
+        : float
+          ephemeris start time of the image
+        """
+        return spice.sct2e(self.spacecraft_id, self.spacecraft_clock_stop_count)
+
+
+    @property
+    def sensor_frame_id(self):
+        """
+        Returns the sensor frame id.  Depends on the instrument that was used to
+        capture the image.
+
+        Returns
+        -------
+        : int
+          Sensor frame id
+        """
+        spectra = self.base_band[3]
+        return spice.namfrm(f"LISM_MI_{spectra}_HEAD")
+
+
+    @property
+    def detector_center_line(self):
+        """
+        Returns the center detector line of the detector. Expects tc_id to be
+        defined. This should be a string of the form LISM_MI1 or LISM_MI2.
+
+        We subtract 0.5 from the center line because as per the IK:
+        Center of the first pixel is defined as "1.0".
+
+        Returns
+        -------
+        : int
+          The detector line of the principle point
+        """
+        return spice.gdpool('INS{}_CENTER'.format(self.ikid), 0, 2)[1] - 0.5
+
+    @property
+    def detector_center_sample(self):
+        """
+        Returns the center detector sample of the detector. Expects tc_id to be
+        defined. This should be a string of the form LISM_MI1 or LISM_MI2.
+
+        We subtract 0.5 from the center sample because as per the IK:
+        Center of the first pixel is defined as "1.0".
+
+        Returns
+        -------
+        : int
+          The detector sample of the principle point
+        """
+        return spice.gdpool('INS{}_CENTER'.format(self.ikid), 0, 2)[0] - 0.5
+
+
+    @property
+    def _odkx(self):
+        """
+        Returns the x coefficients of the optical distortion model.
+        Expects tc_id to be defined. This should be a string of the form
+        LISM_MI1 or LISM_MI2.
+
+        Returns
+        -------
+        : list
+          Optical distortion x coefficients
+        """
+        return spice.gdpool('INS{}_DISTORTION_COEF_X'.format(self.ikid),0, 4).tolist()
+
+
+    @property
+    def _odky(self):
+        """
+        Returns the y coefficients of the optical distortion model.
+        Expects tc_id to be defined. This should be a string of the form
+        LISM_MI1 or LISM_MI2.
+
+        Returns
+        -------
+        : list
+          Optical distortion y coefficients
+        """
+        return spice.gdpool('INS{}_DISTORTION_COEF_Y'.format(self.ikid), 0, 4).tolist()
+
+
+    @property
+    def boresight_x(self):
+        """
+        Returns the x focal plane coordinate of the boresight.
+        Expects ikid to be defined. This should be the NAIF integer ID for the
+        sensor.
+
+        Returns
+        -------
+        : float
+          Boresight focal plane x coordinate
+        """
+        return spice.gdpool('INS{}_BORESIGHT'.format(self.ikid), 0, 1)[0]
+
+
+    @property
+    def boresight_y(self):
+        """
+        Returns the y focal plane coordinate of the boresight.
+        Expects ikid to be defined. This should be the NAIF integer ID for the
+        sensor.
+
+        Returns
+        -------
+        : float
+          Boresight focal plane x coordinate
+        """
+        return spice.gdpool('INS{}_BORESIGHT'.format(self.ikid), 1, 1)[0]
+
+
+
+    @property
+    def usgscsm_distortion_model(self):
+        """
+        Kaguya uses a unique radial distortion model so we need to overwrite the
+        method packing the distortion model into the ISD.
+
+        from the IK:
+
+        Line-of-sight vector of pixel no. n can be expressed as below.
+
+        Distortion coefficients information:
+        INS<INSTID>_DISTORTION_COEF_X  = ( a0, a1, a2, a3)
+        INS<INSTID>_DISTORTION_COEF_Y  = ( b0, b1, b2, b3),
+
+        Distance r from the center:
+        r = - (n - INS<INSTID>_CENTER) * INS<INSTID>_PIXEL_SIZE.
+
+        Line-of-sight vector v is calculated as
+        v[X] = INS<INSTID>BORESIGHT[X] + a0 + a1*r + a2*r^2 + a3*r^3 ,
+        v[Y] = INS<INSTID>BORESIGHT[Y] + r+a0 + a1*r +a2*r^2 + a3*r^3 ,
+        v[Z] = INS<INSTID>BORESIGHT[Z]
+
+        Expects odkx and odky to be defined. These should be a list of optical
+        distortion x and y coefficients respectively.
+
+        Returns
+        -------
+        : dict
+          radial distortion model
+
+        """
+        return {
+            "kaguyalism": {
+                "x" : self._odkx,
+                "y" : self._odky,
+                "boresight_x" : self.boresight_x,
+                "boresight_y" : self.boresight_y
+            }
+        }
+
+
+    @property
+    def line_exposure_duration(self):
+        """
+        Returns Line Exposure Duration
+
+        Kaguya has an unintuitive key for this called CORRECTED_SAMPLING_INTERVAL.
+        The original LINE_EXPOSURE_DURATION PDS3 keys is often incorrect and cannot
+        be trusted.
+
+        Returns
+        -------
+        : float
+          Line exposure duration
+        """
+        # It's a list, but only sometimes.
+        # seems to depend on whether you are using the original zipped archives or
+        # if its downloaded from Jaxa's image search:
+        # (https://darts.isas.jaxa.jp/planet/pdap/selene/product_search.html#)
+        try:
+            return self.label['IsisCube']['Instrument']['CorrectedSamplingInterval'][0].value * 0.001 # Scale to seconds
+        except:
+            return self.label['IsisCube']['Instrument']['CorrectedSamplingInterval'].value * 0.001  # Scale to seconds
+
+
+
+    @property
+    def focal2pixel_samples(self):
+        """
+        Calculated using 1/pixel pitch
+        Expects ikid to be defined. This should be the NAIF integer ID code
+        for the sensor.
+
+        Returns
+        -------
+        : list
+          focal plane to detector samples
+        """
+        pixel_size = spice.gdpool('INS{}_PIXEL_SIZE'.format(self.ikid), 0, 1)[0]
+        return [0, 0, -1/pixel_size]
+
+
+    @property
+    def focal2pixel_lines(self):
+        """
+        Calculated using 1/pixel pitch
+        Expects ikid to be defined. This should be the NAIF integer ID code
+        for the sensor.
+
+        Returns
+        -------
+        : list
+          focal plane to detector lines
+        """
+        pixel_size = spice.gdpool('INS{}_PIXEL_SIZE'.format(self.ikid), 0, 1)[0]
+        return [0, 1/pixel_size, 0]
+
+
+    def spacecraft_direction(self):
+        """
+        Gets the moving direction of the spacecraft from the label, where -1 is moving
+        as intended and 1 is moving inverted.
+
+        Returns
+        -------
+        : int
+          Moving direction of the spacecraft
+        """
+        return int(self.label['SatelliteMovingDirection'])
