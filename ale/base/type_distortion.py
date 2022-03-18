29a0bd60d773700c691feeed9766bcac826a2b54
Austin Sanders <austinsanders1993@gmail.com>
2022-03-18 14:12:26 -0700

    Added legendre distortion model

--- ale/base/type_distortion.py
+++ ale/base/type_distortion.py
@@ -1,3 +1,27 @@
+class LegendreDistortion():
+    """
+    Mix-in for sensors that use a legendre distortion model.
+    """
+
+    @property
+    def usgscsm_distortion_model(self):
+        """
+        Expects odtx and odty to be defined. These should be lists containing
+        the legendre distortion coefficients
+
+        Returns
+        -------
+        : dict
+          Dictionary containing the usgscsm distortion model
+        """
+        return {
+            "legendre":{
+                "x_coefficients" : self.odtx,
+                "y_coefficients" : self.odty
+            }
+        }
+
+
 class RadialDistortion():
     """
     Mix-in for sensors that use a radial distortion model.
