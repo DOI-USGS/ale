92c1cf15a6bbdffcb9cb87332941b9d391a46cad
acpaquette <acpaquette@usgs.gov>
2022-06-10 17:09:47 -0700

    HiRISE IsisLabel NaifSpice Driver (#471)
    * Added initial working mro hirise driver
    
    * Fixed error messaging in frame trace
    
    * Added mro hirise tests

--- ale/drivers/mro_drivers.py
+++ ale/drivers/mro_drivers.py
@@ -8,6 +8,8 @@ from ale.base.label_isis import IsisLabel
 from ale.base.type_distortion import RadialDistortion
 from ale.base.type_sensor import LineScanner
 
+from ale import util
+
 
 class MroCtxIsisLabelIsisSpiceDriver(LineScanner, IsisLabel, IsisSpice, RadialDistortion, Driver):
 
@@ -255,3 +257,145 @@ class MroCtxPds3LabelNaifSpiceDriver(LineScanner, Pds3Label, NaifSpice, RadialDi
           platform name
         """
         return self.label['SPACECRAFT_NAME']
+
+hirise_ccd_lookup = {
+  0: 0,
+  1: 1,
+  2: 2,
+  3: 3,
+  4: 12,
+  5: 4,
+  6: 10,
+  7: 11,
+  8: 5,
+  9: 13,
+  10: 6,
+  11: 7,
+  12: 8,
+  13: 9
+}
+
+class MroHiRiseIsisLabelNaifSpiceDriver(LineScanner, IsisLabel, NaifSpice, RadialDistortion, Driver):
+    """
+    Driver for reading HiRise ISIS labels.
+    """
+
+    @property
+    def instrument_id(self):
+        """
+        Returns an instrument id for uniquely identifying the instrument, but often
+        also used to be piped into Spice Kernels to acquire IKIDs. Therefore they
+        the same ID the Spice expects in bods2c calls.
+        Expects instrument_id to be defined in the IsisLabel mixin. This should be
+        a string of the form 'CTX'
+
+        Returns
+        -------
+        : str
+          instrument id
+        """
+        id_lookup = {
+            "HIRISE" : "MRO_HIRISE"
+        }
+        return id_lookup[super().instrument_id]
+
+    @property
+    def sensor_name(self):
+        """
+        ISIS doesn't propergate this to the ingested cube label, so hard-code it.
+        """
+        return "HIRISE CAMERA"
+
+
+    @property
+    def un_binned_rate(self):
+        if not hasattr(self, "_un_binned_rate"):
+            delta_line_timer_count = self.label["IsisCube"]["Instrument"]["DeltaLineTimerCount"]
+            self._un_binned_rate = (74.0 + (delta_line_timer_count / 16.0)) / 1000000.0
+        return self._un_binned_rate
+
+    @property
+    def ephemeris_start_time(self):
+        if not hasattr(self, "_ephemeris_start_time"):
+            tdi_mode = self.label["IsisCube"]["Instrument"]["Tdi"]
+            bin_mode = self.label["IsisCube"]["Instrument"]["Summing"]
+            # Code replicated from the ISIS HiRise Camera Model
+
+            # The -74999 is the code to select the transformation from
+            # high-precision MRO SCLK to ET
+            start_time = spice.scs2e(-74999, self.spacecraft_clock_start_count)
+            # Adjust the start time so that it is the effective time for
+            # the first line in the image file.  Note that on 2006-03-29, this
+            # time is now subtracted as opposed to adding it.  The computed start
+            # time in the EDR is at the first serial line.
+            start_time -= self.un_binned_rate * ((tdi_mode / 2.0) - 0.5);
+            # Effective observation
+            # time for all the TDI lines used for the
+            # first line before doing binning
+            start_time += self.un_binned_rate * ((bin_mode / 2.0) - 0.5);
+            self._ephemeris_start_time = start_time
+        return self._ephemeris_start_time
+
+    @property
+    def exposure_duration(self):
+        if not hasattr(self, "_exposure_duration"):
+            self._exposure_duration = self.un_binned_rate * self.label["IsisCube"]["Instrument"]["Summing"]
+        return self._exposure_duration
+
+    @property
+    def ccd_ikid(self):
+        ccd_number = hirise_ccd_lookup[self.label["IsisCube"]["Instrument"]["CpmmNumber"]]
+        return spice.bods2c("MRO_HIRISE_CCD{}".format(ccd_number))
+
+    @property
+    def sensor_frame_id(self):
+        return -74690
+
+    @property
+    def detector_center_sample(self):
+        """
+        Returns the center detector sample. Expects ikid to be defined. This should
+        be an integer cont?aining the Naif Id code of the instrument.
+
+        Returns
+        -------
+        : float
+          Detector sample of the principal point
+        """
+        return 0
+
+    @property
+    def detector_center_line(self):
+        """
+        Returns the center detector line. Expects ikid to be defined. This should
+        be an integer containing the Naif Id code of the instrument.
+
+        Returns
+        -------
+        : float
+          Detector line of the principal point
+        """
+        return 0
+
+    @property
+    def naif_keywords(self):
+        """
+        Updated set of naif keywords containing the NaifIkCode for the specific
+        Juno filter used when taking the image.
+
+        Returns
+        -------
+        : dict
+          Dictionary of keywords and values that ISIS creates and attaches to the label
+        """
+        return {**super().naif_keywords, **util.query_kernel_pool(f"*{self.ccd_ikid}*")}
+
+    @property
+    def sensor_model_version(self):
+        """
+        Returns
+        -------
+        : int
+          ISIS sensor model version
+        """
+        return 1
--- tests/pytests/test_mro_drivers.py
+++ tests/pytests/test_mro_drivers.py
@@ -8,31 +8,36 @@ import numpy as np
 import spiceypy as spice
 
 import ale
-from ale.drivers.mro_drivers import MroCtxPds3LabelNaifSpiceDriver, MroCtxIsisLabelNaifSpiceDriver, MroCtxIsisLabelIsisSpiceDriver
+from ale.drivers.mro_drivers import MroCtxPds3LabelNaifSpiceDriver, MroCtxIsisLabelNaifSpiceDriver, MroCtxIsisLabelIsisSpiceDriver, MroHiRiseIsisLabelNaifSpiceDriver
 
 from conftest import get_image, get_image_kernels, get_isd, convert_kernels, get_image_label, compare_dicts
 
-#@pytest.fixture
-# def ctx_isf
-
 @pytest.fixture(scope='module')
-def test_kernels():
+def test_ctx_kernels():
     kernels = get_image_kernels('B10_013341_1010_XN_79S172W')
     updated_kernels, binary_kernels = convert_kernels(kernels)
     yield updated_kernels
     for kern in binary_kernels:
         os.remove(kern)
 
+@pytest.fixture(scope='module')
+def test_hirise_kernels():
+    kernels = get_image_kernels('PSP_001446_1790_BG12_0')
+    updated_kernels, binary_kernels = convert_kernels(kernels)
+    yield updated_kernels
+    for kern in binary_kernels:
+        os.remove(kern)
+
 @pytest.mark.parametrize("label_type, kernel_type", [('pds3', 'naif'), ('isis3', 'naif'), ('isis3', 'isis')])
-def test_mro_load(test_kernels, label_type, kernel_type):
+def test_mro_ctx_load(test_ctx_kernels, label_type, kernel_type):
     label_file = get_image_label('B10_013341_1010_XN_79S172W', label_type)
 
-    if label_type == 'isis3' and kernel_type == 'isis': 
+    if label_type == 'isis3' and kernel_type == 'isis':
         label_file = get_image('B10_013341_1010_XN_79S172W')
         isd_str = ale.loads(label_file)
         compare_isd = get_isd('ctx_isis')
-    else: 
-        isd_str = ale.loads(label_file, props={'kernels': test_kernels})
+    else:
+        isd_str = ale.loads(label_file, props={'kernels': test_ctx_kernels})
         compare_isd = get_isd('ctx')
 
     isd_obj = json.loads(isd_str)
@@ -42,8 +47,20 @@ def test_mro_load(test_kernels, label_type, kernel_type):
 
     assert compare_dicts(isd_obj, compare_isd) == []
 
-# ========= Test isislabel and isisspice driver =========
-class test_isis_isis(unittest.TestCase):
+@pytest.mark.parametrize("label_type, kernel_type", [('isis3', 'naif')])
+def test_mro_hirise_load(test_hirise_kernels, label_type, kernel_type):
+    label_file = get_image_label("PSP_001446_1790_BG12_0", label_type)
+
+    isd_str = ale.loads(label_file, props={'kernels': test_hirise_kernels})
+    compare_isd = get_isd('hirise')
+
+    isd_obj = json.loads(isd_str)
+    comparison = compare_dicts(isd_obj, compare_isd)
+    print(comparison)
+    assert comparison == []
+
+# ========= Test ctx isislabel and isisspice driver =========
+class test_ctx_isis_isis(unittest.TestCase):
 
     def setUp(self):
         label = get_image_label("B10_013341_1010_XN_79S172W", "isis3")
@@ -61,8 +78,8 @@ class test_isis_isis(unittest.TestCase):
     def test_detector_center_sample(self):
         assert self.driver.detector_center_sample == 2542.96099
 
-# ========= Test isislabel and naifspice driver =========
-class test_isis_naif(unittest.TestCase):
+# ========= Test ctx isislabel and naifspice driver =========
+class test_ctx_isis_naif(unittest.TestCase):
 
     def setUp(self):
         label = get_image_label("B10_013341_1010_XN_79S172W", "isis3")
@@ -99,8 +116,8 @@ class test_isis_naif(unittest.TestCase):
     def test_sensor_model_version(self):
         assert self.driver.sensor_model_version == 1
 
-# ========= Test pds3label and naifspice driver =========
-class test_pds_naif(unittest.TestCase):
+# ========= Test ctx pds3label and naifspice driver =========
+class test_ctx_pds_naif(unittest.TestCase):
 
     def setUp(self):
         label = get_image_label("B10_013341_1010_XN_79S172W", "pds3")
@@ -126,3 +143,45 @@ class test_pds_naif(unittest.TestCase):
 
     def test_platform_name(self):
         assert self.driver.platform_name == "MARS_RECONNAISSANCE_ORBITER"
+
+
+# ========= Test hirise isislabel and naifspice driver =========
+class test_hirise_isis_naif(unittest.TestCase):
+
+    def setUp(self):
+        label = get_image_label("PSP_001446_1790_BG12_0", "isis3")
+        self.driver = MroHiRiseIsisLabelNaifSpiceDriver(label)
+
+    def test_instrument_id(self):
+        assert self.driver.instrument_id == "MRO_HIRISE"
+
+    def test_sensor_name(self):
+        assert self.driver.sensor_name == "HIRISE CAMERA"
+
+    def test_un_binned_rate(self):
+        assert self.driver.un_binned_rate == 0.0000836875
+
+    def test_ephemeris_start_time(self):
+        with patch('ale.drivers.mro_drivers.spice.scs2e', return_value=12345) as scs2e:
+            assert self.driver.ephemeris_start_time == 12344.997489375
+            scs2e.assert_called_with(-74999, '848201291:62546')
+
+    def test_exposure_duration(self):
+        assert self.driver.exposure_duration == 0.00033475
+
+    def test_ccd_ikid(self):
+        with patch('ale.drivers.mro_drivers.spice.bods2c', return_value=12345) as bods2c:
+            assert self.driver.ccd_ikid == 12345
+            bods2c.assert_called_with('MRO_HIRISE_CCD12')
+
+    def test_sensor_frame_id(self):
+        assert self.driver.sensor_frame_id == -74690
+
+    def test_detector_center_sample(self):
+        assert self.driver.detector_center_sample == 0
+
+    def test_detector_center_line(self):
+        assert self.driver.detector_center_sample == 0
+
+    def test_sensor_model_version(self):
+        assert self.driver.sensor_model_version == 1
