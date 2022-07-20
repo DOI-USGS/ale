650d42ee9944cf8cc617032bd003975c1af4445f
Amy Stamile <74275278+amystamile-usgs@users.noreply.github.com>
2022-07-20 07:15:21 -0700

    Leisa driver (#474)
    * Initial Leisa Driver
    
    * Updated nh tests
    
    * Added sensor_name
    
    * Removed Jupiter test data and added Pluto test data.
    
    * Removed light time correction and increased padding in KernelSlice
    
    * Fixed exposure and increased padding in KernelSlice
    
    * fixed exposure duration int error
    
    * Fixes CI failures. Updated isd.
    
    * separated load tests for lorri and leisa.
    
    * Added Leisa unit tests

--- ale/drivers/nh_drivers.py
+++ ale/drivers/nh_drivers.py
@@ -11,6 +11,7 @@ from ale.base.type_distortion import NoDistortion
 from ale.base.data_naif import NaifSpice
 from ale.base.label_isis import IsisLabel
 from ale.base.type_sensor import Framer
+from ale.base.type_sensor import LineScanner
 
 class NewHorizonsLorriIsisLabelNaifSpiceDriver(Framer, IsisLabel, NaifSpice, NoDistortion, Driver):
     """
@@ -65,3 +66,125 @@ class NewHorizonsLorriIsisLabelNaifSpiceDriver(Framer, IsisLabel, NaifSpice, NoD
     @property
     def sensor_name(self):
         return self.label['IsisCube']['Instrument']['SpacecraftName']
+
+
+class NewHorizonsLeisaIsisLabelNaifSpiceDriver(LineScanner, IsisLabel, NaifSpice, NoDistortion, Driver):
+    """
+    Driver for reading New Horizons LEISA ISIS3 Labels.
+    """
+
+    @property
+    def instrument_id(self):
+        """
+        Returns an instrument id for uniquely identifying the instrument, but often
+        also used to be piped into Spice Kernels to acquire IKIDs. Therefore they
+        the same ID the Spice expects in bods2c calls.
+
+        Returns
+        -------
+        : str
+          instrument id
+        """
+        id_lookup = {
+            "LEISA" : "NH_RALPH_LEISA"
+        }
+        return id_lookup[super().instrument_id]
+
+    @property
+    def ikid(self):
+        """
+        Overridden to grab the ikid from the Isis Cube since there is no way to
+        obtain this value with a spice bods2c call. Isis sets this value during
+        ingestion, based on the original fits file.
+
+        Returns
+        -------
+        : int
+          Naif ID used to for identifying the instrument in Spice kernels
+        """
+        return self.label['IsisCube']['Kernels']['NaifFrameCode'][0]
+
+
+    @property
+    def ephemeris_start_time(self):
+        """
+        Returns the ephemeris start time of the image.
+        Expects spacecraft_id to be defined. This should be the integer
+        Naif ID code for the spacecraft.
+
+        Returns
+        -------
+        : float
+          ephemeris start time of the image
+        """
+        return spice.scs2e(self.spacecraft_id, self.spacecraft_clock_start_count)
+
+    @property
+    def ephemeris_stop_time(self):
+        """
+        ISIS doesn't preserve the spacecraft stop count that we can use to get
+        the ephemeris stop time of the image, so compute the ephemeris stop time
+        from the start time and the exposure duration.
+        """
+        return self.ephemeris_start_time + self.exposure_duration * self.image_lines
+
+    @property
+    def detector_center_line(self):
+        """
+        Returns the center detector line. Expects ikid to be defined. This should
+        be an integer containing the Naif Id code of the instrument.
+
+        Returns
+        -------
+        : float
+          Detector line of the principal point
+        """
+        return 0
+
+    @property
+    def detector_center_sample(self):
+        """
+        Returns the center detector sample. Expects ikid to be defined. This should
+        be an integer containing the Naif Id code of the instrument.
+
+        Returns
+        -------
+        : float
+          Detector sample of the principal point
+        """
+        return 0
+
+    @property
+    def sensor_name(self):
+        """
+        Returns the name of the instrument. Need to over-ride isis_label because
+        InstrumentName is not defined in the ISIS label for NH Leisa cubes.
+
+        Returns
+        -------
+        : str
+        Name of the sensor
+        """
+        return self.instrument_id
+
+    @property
+    def sensor_model_version(self):
+        """
+        Returns
+        -------
+        : int
+          ISIS sensor model version
+        """
+        return 1
+
+    @property
+    def exposure_duration(self):
+        """
+        The exposure duration of the image, in seconds
+
+        Returns
+        -------
+        : float
+          Exposure duration in seconds
+        """
+        return self.label['IsisCube']['Instrument']['ExposureDuration']
--- tests/pytests/test_newhorizons_drivers.py
+++ tests/pytests/test_newhorizons_drivers.py
@@ -14,21 +14,78 @@ from unittest.mock import patch
 
 from conftest import get_image_label, get_image_kernels, convert_kernels, compare_dicts
 
-from ale.drivers.nh_drivers import NewHorizonsLorriIsisLabelNaifSpiceDriver
+from ale.drivers.nh_drivers import NewHorizonsLorriIsisLabelNaifSpiceDriver, NewHorizonsLeisaIsisLabelNaifSpiceDriver
 from conftest import get_image_kernels, convert_kernels, get_image_label, get_isd
+
+image_dict = {
+    'lor_0034974380_0x630_sci_1': get_isd("nhlorri"),
+    'lsb_0296962438_0x53c_eng': get_isd("nhleisa")
+}
+
 
 @pytest.fixture()
 def test_kernels(scope="module"):
-    kernels = get_image_kernels("lor_0034974380_0x630_sci_1")
-    updated_kernels, binary_kernels = convert_kernels(kernels)
+    updated_kernels = {}
+    binary_kernels = {}
+    for image in image_dict.keys():
+        kernels = get_image_kernels(image)
+        updated_kernels[image], binary_kernels[image] = convert_kernels(kernels)
     yield updated_kernels
-    for kern in binary_kernels:
-        os.remove(kern)
+    for kern_list in binary_kernels.values():
+        for kern in kern_list:
+            os.remove(kern)
 
-def test_newhorizons_load(test_kernels):
-    label_file = get_image_label("lor_0034974380_0x630_sci_1", "isis")
-    isd_str = ale.loads(label_file, props={'kernels': test_kernels})
+# Test load of nh lorri labels
+@pytest.mark.parametrize("image", ['lor_0034974380_0x630_sci_1'])
+def test_nhlorri_load(test_kernels, image):
+    label_file = get_image_label(image, 'isis')
+    isd_str = ale.loads(label_file, props={'kernels': test_kernels[image]})
+    compare_isd = image_dict[image]
     isd_obj = json.loads(isd_str)
-    compare_dict = get_isd('newhorizons')
     print(json.dumps(isd_obj, indent=2))
-    assert compare_dicts(isd_obj, compare_dict) == []
+    assert compare_dicts(isd_obj, compare_isd) == []
+
+# Test load of nh leisa labels
+@pytest.mark.parametrize("image", ['lsb_0296962438_0x53c_eng'])
+def test_nhleisa_load(test_kernels, image):
+    label_file = get_image_label(image, 'isis')
+    isd_str = ale.loads(label_file, props={'kernels': test_kernels[image]})
+    compare_isd = image_dict[image]
+    isd_obj = json.loads(isd_str)
+    print(json.dumps(isd_obj, indent=2))
+    assert compare_dicts(isd_obj, compare_isd) == []
+
+
+# ========= Test Leisa isislabel and naifspice driver =========
+class test_leisa_isis_naif(unittest.TestCase):
+    def setUp(self):
+        label = get_image_label("lsb_0296962438_0x53c_eng", "isis")
+        self.driver = NewHorizonsLeisaIsisLabelNaifSpiceDriver(label)
+
+    def test_instrument_id(self):
+        assert self.driver.instrument_id == "NH_RALPH_LEISA"
+
+    def test_ikid(self):
+            assert self.driver.ikid == -98901
+
+    def test_ephemeris_start_time(self):
+        with patch('ale.drivers.nh_drivers.spice.scs2e', return_value=12345) as scs2e:
+            assert self.driver.ephemeris_start_time == 12345
+            scs2e.assert_called_with(-98, '0296962438:00000')
+
+    def test_ephemeris_stop_time(self):
+        with patch('ale.drivers.nh_drivers.spice.scs2e', return_value=12345) as scs2e:
+            assert self.driver.ephemeris_stop_time == (12345 + self.driver.exposure_duration * self.driver.image_lines)
+            scs2e.assert_called_with(-98, '0296962438:00000')
+
+    def test_detector_center_sample(self):
+        assert self.driver.detector_center_sample == 0
+
+    def test_detector_center_line(self):
+        assert self.driver.detector_center_line == 0
+
+    def test_sensor_name(self):
+        assert self.driver.sensor_name == "NH_RALPH_LEISA"
+
+    def test_exposure_duration(self):
+        np.testing.assert_almost_equal(self.driver.exposure_duration, 0.856)
