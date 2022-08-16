ee9f02e6b6ad7a0fd1b94c270e3bad1056d03eab
Austin Sanders <arsanders@usgs.gov>
2022-08-16 16:26:13 -0600

    Address PR feedback

--- tests/pytests/test_newhorizons_drivers.py
+++ tests/pytests/test_newhorizons_drivers.py
@@ -35,33 +35,6 @@ def test_kernels(scope="module"):
         for kern in kern_list:
             os.remove(kern)
 
-# Test load of newhorizons labels
-@pytest.mark.parametrize("image", ["mc3_0295574631_0x536_sci"])
-def test_nhmvic_load(test_kernels, image):
-    label_file = get_image_label(image, 'isis')
-    isd_str = ale.loads(label_file, props={'kernels': test_kernels[image],
-                                           'exact_ck_times': False})
-    compare_isd = image_dict[image]
-
-    isd_obj = json.loads(isd_str)
-    assert compare_dicts(isd_obj, compare_isd) == []
-
-
-class test_mvic_isis3_naif(unittest.TestCase):
-
-    def setUp(self):
-        label = get_image_label("mc3_0295574631_0x536_sci", "isis")
-        self.driver = NewHorizonsMvicIsisLabelNaifSpiceDriver(label)
-
-    def test_instrument_id(self):
-        assert self.driver.instrument_id == 'NH_MVIC'
-
-    def test_ikid(self):
-        assert self.driver.ikid == -98908
-
-    def test_sensor_model_version(self):
-        assert self.driver.sensor_model_version == 1
-
 # Test load of nh lorri labels
 @pytest.mark.parametrize("image", ['lor_0034974380_0x630_sci_1'])
 def test_nhlorri_load(test_kernels, image):
@@ -126,7 +99,7 @@ class test_leisa_isis_naif(unittest.TestCase):
     def test_exposure_duration(self):
         np.testing.assert_almost_equal(self.driver.exposure_duration, 0.856)
 
-class test_mvic_isis3_naif(unittest.TestCase):
+class test_mvic_framer_isis3_naif(unittest.TestCase):
 
     def setUp(self):
         label = get_image_label("mpf_0295610274_0x539_sci", "isis")
@@ -140,3 +113,27 @@ class test_mvic_isis3_naif(unittest.TestCase):
 
     def test_sensor_model_version(self):
         assert self.driver.sensor_model_version == 1
+
+    def test_ephemeris_start_time(self):
+        with patch('ale.drivers.nh_drivers.spice.utc2et', return_value=12345) as utc2et:
+            assert self.driver.ephemeris_start_time == 12345
+            utc2et.assert_called_with("2015-06-03 04:06:32.848000")
+
+    def test_ephemeris_stop_time(self):
+        with patch('ale.drivers.nh_drivers.spice.utc2et', return_value=12345) as utc2et:
+            assert self.driver.ephemeris_start_time == 12345
+            utc2et.assert_called_with("2015-06-03 04:06:32.848000")
+
+    def test_detector_center_line(self):
+        assert self.driver.detector_center_line == -1
+
+    def test_detector_center_sample(self):
+        assert self.driver.detector_center_sample == 0
+
+    def test_sensor_name(self):
+        assert self.driver.sensor_name == 'NEW HORIZONS'
+
+    def test_band_times(self):
+        with patch('ale.drivers.nh_drivers.spice.utc2et', return_value=12345) as utc2et:
+            assert self.driver.ephemeris_start_time == 12345
+            utc2et.assert_called_with("2015-06-03 04:06:32.848000")
