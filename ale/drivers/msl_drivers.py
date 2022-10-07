b52a869c94b4e2fdc85ae66827f9321208744827
Amy Stamile <astamile@usgs.gov>
2022-10-07 09:23:38 -0700

    added docs

--- ale/drivers/msl_drivers.py
+++ ale/drivers/msl_drivers.py
@@ -7,10 +7,33 @@ from ale.base.base import Driver
 class MslMastcamPds3NaifSpiceDriver(Framer, Pds3Label, NaifSpice, NoDistortion, Driver):
     @property
     def spacecraft_name(self):
+      """
+      Spacecraft name used in various SPICE calls to acquire
+      ephemeris data. MSI Mastcam img PDS3 labels do not the have a SPACECRAFT_NAME keyword,
+      so we override it here to find INSTRUMENT_HOST_NAME in the label.
+
+      Returns
+      -------
+      : str
+        Spacecraft name
+      """
       return self.instrument_host_name
     
     @property
     def instrument_id(self):
+      """
+      Returns an instrument id for uniquely identifying the instrument, but often
+      also used to be piped into Spice Kernels to acquire IKIDs. Therefore they
+      the same ID the Spice expects in bods2c calls.
+
+      Expects instrument_id to be defined in the Pds3Label mixin. This should
+      be a string of the form MAST_RIGHT or MAST_LEFT.
+
+      Returns
+      -------
+      : str
+        instrument id
+      """
       lookup = {
         "MAST_RIGHT": 'MASTCAM_RIGHT',
         "MAST_LEFT": 'MASTCAM_LEFT'
@@ -19,6 +42,15 @@ class MslMastcamPds3NaifSpiceDriver(Framer, Pds3Label, NaifSpice, NoDistortion, 
 
     @property
     def exposure_duration(self):
+      """
+      Returns the exposure duration converted to seconds. EXPOSURE_DURATION keyword is found
+      in the INSTRUMENT_STATE_PARMS group of the PDS3 label.
+
+      Returns
+      -------
+      : float
+        Returns the exposure duration in seconds from the PDS3 label.
+      """
       try:
         unit = self.label['INSTRUMENT_STATE_PARMS']['EXPOSURE_DURATION'].units
         unit = unit.lower()
