3f49ebf10bd4c0b8158a3e63477148c6a3d49ac6
Jacob Cain <jrcain@usgs.gov>
2023-02-16 11:28:53 -0800

    amica tests (load test fails), partial NIRS driver

--- ale/drivers/hayabusa_drivers.py
+++ ale/drivers/hayabusa_drivers.py
@@ -4,18 +4,34 @@ import ale
 from ale.base.data_naif import NaifSpice
 from ale.base.label_isis import IsisLabel
 from ale.base.type_sensor import Framer
-from ale.base.type_distortion import RadialDistortion
+from ale.base.type_distortion import RadialDistortion, NoDistortion
 from ale.base.base import Driver
 
 class HayabusaAmicaIsisLabelNaifSpiceDriver(Framer, IsisLabel, NaifSpice, RadialDistortion, Driver):
 
     @property
     def instrument_id(self):
+        """
+        Returns the ID of the instrument
+
+        Returns
+        -------
+        : str
+          Name of the instrument
+        """
         lookup_table = {'AMICA': 'HAYABUSA_AMICA'}
         return lookup_table[super().instrument_id]
 
     @property
     def sensor_model_version(self):
+        """
+        The ISIS Sensor model number for HiRise in ISIS. This is likely just 1
+
+        Returns
+        -------
+        : int
+          ISIS sensor model version
+        """
         return 1
 
     @property
@@ -41,7 +57,7 @@ class HayabusaAmicaIsisLabelNaifSpiceDriver(Framer, IsisLabel, NaifSpice, Radial
         : str
           Spacecraft clock start count
         """
-        return self.label['IsisCube']['Instrument']['SpacecraftClockStartCount'].value
+        return str(self.label['IsisCube']['Instrument']['SpacecraftClockStartCount'].value)
 
     @property
     def spacecraft_clock_stop_count(self):
@@ -54,4 +70,66 @@ class HayabusaAmicaIsisLabelNaifSpiceDriver(Framer, IsisLabel, NaifSpice, Radial
         : str
           Spacecraft clock stop count
         """
-        return self.label['IsisCube']['Instrument']['SpacecraftClockStopCount'].value
+        return str(self.label['IsisCube']['Instrument']['SpacecraftClockStopCount'].value)
+
+
+class HayabusaNirsIsisLabelNaifSpiceDriver(Framer, IsisLabel, NaifSpice, NoDistortion, Driver):
+
+    @property
+    def instrument_id(self):
+        """
+        Returns the ID of the instrument
+
+        Returns
+        -------
+        : str
+          Name of the instrument
+        """
+        lookup_table = {'NIRS': 'HAYABUSA_NIRS'}
+        return lookup_table[super().instrument_id]
+
+    @property
+    def sensor_model_version(self):
+        """
+        The ISIS Sensor model number for HiRise in ISIS. This is likely just 1
+
+        Returns
+        -------
+        : int
+          ISIS sensor model version
+        """
+        return 1
+
+    @property
+    def sensor_name(self):
+        """
+        Returns the name of the instrument
+
+        Returns
+        -------
+        : str
+          Name of the sensor
+        """
+        return self.instrument_id
+    
+    @property
+    def exposure_duration(self):
+        """
+        Returns the exposure duration of the instrument
+
+        Returns
+        -------
+        : str
+          Exposure Duration
+        """
+
+        """
+        print("** DEBUG EXPOSURE DURATION **")
+        print("Total Integration Time: " + str(self.label['IsisCube']['Instrument']['TotalIntegrationTime']))
+        print("Ephemeris Start Time: " + str(self.ephemeris_start_time))
+        print("Start Time TIMOUT: " + str(spice.timout(self.ephemeris_start_time, "MON DD, YYYY  HR:MN:SC.####", 65)))
+        ephemeris_stop_time = spice.scs2e(self.spacecraft_id, self.spacecraft_clock_stop_count)
+        print("Stop Time TIMOUT:  " + str(spice.timout(ephemeris_stop_time, "MON DD, YYYY  HR:MN:SC.####", 65)))
+        """
+
+        return self.label['IsisCube']['Instrument']['TotalIntegrationTime'].value
---
+++ tests/pytests/test_hayabusa_drivers.py
@@ -0,0 +1,52 @@
+import pytest
+import numpy as np
+import os
+import unittest
+from unittest.mock import MagicMock, PropertyMock, patch
+import spiceypy as spice
+import json
+
+from conftest import get_image, get_image_label, get_isd, get_image_kernels, convert_kernels, compare_dicts
+import ale
+from ale.drivers.hayabusa_drivers import HayabusaAmicaIsisLabelNaifSpiceDriver
+from ale import util
+
+
+@pytest.fixture(scope='module')
+def test_amica_kernels():
+    kernels = get_image_kernels('st_2458542208_v')
+    updated_kernels, binary_kernels = convert_kernels(kernels)
+    yield updated_kernels
+    for kern in binary_kernels:
+        os.remove(kern)
+
+def test_amica_load(test_amica_kernels):
+    label_file = get_image_label('st_2458542208_v', 'isis')
+    compare_dict = get_isd("hayabusaamica")
+
+    isd_str = ale.loads(label_file, props={'kernels': test_amica_kernels}, verbose=True)
+    isd_obj = json.loads(isd_str)
+    print(json.dumps(isd_obj, indent=2))
+    print(compare_dicts(isd_obj, compare_dict))
+    assert compare_dicts(isd_obj, compare_dict) == []
+
+
+class test_nac_isis_naif(unittest.TestCase):
+    def setUp(self):
+        label = get_image_label("st_2458542208_v", "isis")
+        self.driver = HayabusaAmicaIsisLabelNaifSpiceDriver(label)
+
+    def test_instrument_id(self):
+        assert self.driver.instrument_id == "HAYABUSA_AMICA"
+    
+    def test_sensor_model_version(self):
+        assert self.driver.sensor_model_version == 1
+
+    def test_sensor_name(self):
+        assert self.driver.sensor_name == "HAYABUSA_AMICA"
+
+    def test_spacecraft_clock_start_count(self):
+        assert self.driver.spacecraft_clock_stop_count == '2457499873'
+
+    def test_spacecraft_clock_stop_count(self):
+        assert self.driver.spacecraft_clock_start_count == '2457499394'
