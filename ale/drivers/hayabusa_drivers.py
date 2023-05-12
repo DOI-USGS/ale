5dc6c5a3d2c60784e2ae48ffa70fe1fcd2d06d16
Jacob Cain <jrcain@usgs.gov>
2023-05-11 18:13:30 -0700

    Added NIRS driver

--- ale/drivers/hayabusa_drivers.py
+++ ale/drivers/hayabusa_drivers.py
@@ -43,3 +43,68 @@ class HayabusaAmicaIsisLabelNaifSpiceDriver(Framer, IsisLabel, NaifSpice, Radial
           Name of the sensor
         """
         return self.instrument_id
+    
+class HayabusaNirsIsisLabelNaifSpiceDriver(Framer, IsisLabel, NaifSpice, NoDistortion, Driver):
+
+    @property
+    def instrument_id(self):
+        """
+        Returns the ID of the instrument
+
+        Returns
+        -------
+        : str
+          Name of the instrument
+        """
+        lookup_table = {'NIRS': 'HAYABUSA_NIRS'}
+        return lookup_table[super().instrument_id]
+
+    @property
+    def sensor_model_version(self):
+        """
+        The ISIS Sensor model number for Hayabusa Nirs in ISIS. This is likely just 1
+
+        Returns
+        -------
+        : int
+          ISIS sensor model version
+        """
+        return 1
+
+    @property
+    def sensor_name(self):
+        """
+        Returns the name of the instrument
+
+        Returns
+        -------
+        : str
+          Name of the sensor
+        """
+        return self.instrument_id
+    
+    @property
+    def ephemeris_stop_time(self):
+        """
+        Returns the exposure duration of the instrument
+
+        Returns
+        -------
+        : str
+          Exposure Duration
+        """
+        
+        return spice.scs2e(self.spacecraft_id, self.spacecraft_clock_stop_count)
+    
+    @property
+    def exposure_duration(self):
+        """
+        Returns the exposure duration of the instrument
+
+        Returns
+        -------
+        : str
+          Exposure Duration
+        """
+        
+        return self.ephemeris_stop_time - self.ephemeris_start_time
