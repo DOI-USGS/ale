656ea7d75530d954407d4302b11f034f16e378b2
Jacob Cain <jrcain@usgs.gov>
2023-05-18 10:05:46 -0700

    lo center time and ikid

--- ale/drivers/lo_drivers.py
+++ ale/drivers/lo_drivers.py
@@ -1,3 +1,4 @@
+import spiceypy as spice
 from ale.base.data_naif import NaifSpice
 from ale.base.label_isis import IsisLabel
 from ale.base.type_sensor import Framer
@@ -16,7 +17,20 @@ class LoHighCameraIsisLabelNaifSpiceDriver(Framer, IsisLabel, NaifSpice, RadialD
         : str
           Name of the instrument
         """
-        lookup_table = {'HIGH_CAMERA': 'LUNAR_ORBITER_HIGH_CAMERA'}  # TODO: Different IDs for different LOs? (there are 5!)
+
+        lo_table = {'Lunar Orbiter 1': 'LO1_HIGH_RESOLUTION_CAMERA',
+                    'Lunar Orbiter 2': 'LO2_HIGH_RESOLUTION_CAMERA',
+                    'Lunar Orbiter 3': 'LO3_HIGH_RESOLUTION_CAMERA',
+                    'Lunar Orbiter 4': 'LO4_HIGH_RESOLUTION_CAMERA',
+                    'Lunar Orbiter 5': 'LO5_HIGH_RESOLUTION_CAMERA',
+                    'LUNAR_ORBITER_1': 'LO1_HIGH_RESOLUTION_CAMERA',
+                    'LUNAR_ORBITER_2': 'LO2_HIGH_RESOLUTION_CAMERA',
+                    'LUNAR_ORBITER_3': 'LO3_HIGH_RESOLUTION_CAMERA',
+                    'LUNAR_ORBITER_4': 'LO4_HIGH_RESOLUTION_CAMERA',
+                    'LUNAR_ORBITER_5': 'LO5_HIGH_RESOLUTION_CAMERA'}
+
+        lookup_table = {'High Resolution Camera': lo_table[self.spacecraft_name]}
+
         return lookup_table[super().instrument_id]
 
     @property
@@ -42,3 +56,37 @@ class LoHighCameraIsisLabelNaifSpiceDriver(Framer, IsisLabel, NaifSpice, RadialD
           Name of the sensor
         """
         return self.instrument_id
+    
+    @property
+    def center_ephemeris_time(self):
+        """
+        Returns the ephemeris time of the image.
+        Expects spacecraft_id to be defined. This should be the integer
+        Naif ID code for the spacecraft.
+
+        Returns
+        -------
+        : float
+          ephemeris time of the image
+        """
+        
+        start_time = str(self.label['IsisCube']['Instrument']['StartTime'])
+        if start_time.endswith('000+00:00'):
+            start_time = start_time[:-9]
+
+        self._center_ephemeris_time = spice.str2et(start_time)
+        return self._center_ephemeris_time
+    
+    @property
+    def ikid(self):
+        """
+        Overridden to grab the ikid from the Isis Cube since there is no way to
+        obtain this value with a spice bods2c call. Isis sets this value during
+        ingestion, based on the original IMG file.
+
+        Returns
+        -------
+        : int
+          Naif ID used to for identifying the instrument in Spice kernels
+        """
+        return self.label["IsisCube"]["Kernels"]["NaifFrameCode"]
