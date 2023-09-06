20e180ee6fbd6b9721e0e14d703b572b38618f25
acpaquette <acp263@nau.edu>
2023-09-06 16:00:05 -0700

    Fixed tests

--- ale/isd_generate.py
+++ ale/isd_generate.py
@@ -69,7 +69,7 @@ def main():
     )
     parser.add_argument(
         "-l", "--local",
-        action="store_false",
+        action="store_true",
         help="Generate local spice data, or image that is unaware of itself relative to "
              "target body. This is largely used for landed/rover data."
     )
@@ -103,7 +103,7 @@ def main():
 
     if len(args.input) == 1:
         try:
-            file_to_isd(args.input[0], args.out, kernels=k, log_level=log_level, only_isis_spice=args.only_isis_spice, only_naif_spice=args.only_naif_spice)
+            file_to_isd(args.input[0], args.out, kernels=k, log_level=log_level, only_isis_spice=args.only_isis_spice, only_naif_spice=args.only_naif_spice, local=args.local)
         except Exception as err:
             # Seriously, this just throws a generic Exception?
             sys.exit(f"File {args.input[0]}: {err}")
@@ -117,7 +117,7 @@ def main():
                                        "log_level": log_level, 
                                        "only_isis_spice": args.only_isis_spice, 
                                        "only_naif_spice": args.only_naif_spice,
-                                       "landed": args.landed}
+                                       "local": args.local}
                 ): f for f in args.input
             }
             for f in concurrent.futures.as_completed(futures):
@@ -138,7 +138,7 @@ def file_to_isd(
     log_level=logging.WARNING,
     only_isis_spice=False,
     only_naif_spice=False,
-    landed=False
+    local=False
 ):
     """
     Returns nothing, but acts as a thin wrapper to take the *file* and generate
@@ -162,7 +162,11 @@ def file_to_isd(
     logger.setLevel(log_level)
 
     logger.info(f"Reading: {file}")
-    props = {"landed": landed}
+    props = {}
+
+    if local:
+        props['landed'] = local
+
     if kernels is not None:
         kernels = [str(PurePath(p)) for p in kernels]
         props["kernels"] = kernels
