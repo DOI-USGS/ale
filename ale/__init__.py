a12559b48c0a598149f90f26ba9cdb37e3041c91
Jacob Cain <115182890+jrcain-usgs@users.noreply.github.com>
2025-12-08 09:50:08 -0800

    Isis9patch (#680)
    * importlib metadata patch
    
    * 0.11.1 version bump

--- ale/__init__.py
+++ ale/__init__.py
@@ -1,20 +1,23 @@
 import os
 import warnings
-from pkg_resources import get_distribution, DistributionNotFound
-
+warnings.filterwarnings("ignore")
 
 try:
-    _dist = get_distribution('ale')
-    # Normalize case for Windows systems
-    dist_loc = os.path.normcase(_dist.location)
-    here = os.path.normcase(__file__)
-    if not here.startswith(os.path.join(dist_loc, 'ale')):
-        # not installed, but there is another version that *is*
-        raise DistributionNotFound
-except DistributionNotFound:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    # For Python <3.8
+    try:
+        from importlib_metadata import version, PackageNotFoundError
+    except ImportError:
+        version = None
+        PackageNotFoundError = Exception
+try:
+    if version is not None:
+        __version__ = version("ale")
+    else:
+        __version__ = 'Unknown version (importlib.metadata not available)'
+except PackageNotFoundError:
     __version__ = 'Please install this project with setup.py'
-else:
-    __version__ = _dist.version
 
 try:
     spice_root = os.environ['ALESPICEROOT']
