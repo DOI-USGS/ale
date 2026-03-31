63651c4cfbd9b8b277c321e5e88c66a14583ebad
Jacob Cain <jrcain@usgs.gov>
2026-03-30 17:07:03 -0700

    get_naif_keywords, throw exception if not found

--- ale/base/data_isis.py
+++ ale/base/data_isis.py
@@ -15,6 +15,7 @@ from ale.transformation import FrameChain
 from scipy.interpolate import interp1d, BPoly
 
 from ale.base.label_isis import IsisLabel
+from ale.base import get_naif_keyword
 
 def read_table_data(table_label, cube):
     """
@@ -147,6 +148,11 @@ def rotate_state(table, rotation):
         rotated_vel = None
     return rotated_pos, rotated_vel, ephemeris_times
 
+def get_naif_keyword(self, ale_name, naif_key):
+    val = self.naif_keywords.get(naif_key, None)
+    if val is None:
+        raise Exception('Could not find a value for {} using the NAIF Keyword {}'.format(ale_name, naif_key))
+    return val
 class IsisSpice():
     """
     Mixin class for reading from an ISIS cube that has been spiceinit'd
@@ -350,7 +356,9 @@ class IsisSpice():
         list :
             The center of the CCD formatted as line, sample
         """
-        return self.naif_keywords.get('INS{}_BORESIGHT_SAMPLE'.format(self.ikid), None)
+           
+        return get_naif_keyword(self, 'detector_center_sample', 'INS{}_BORESIGHT_SAMPLE'.format(self.ikid))
+        #return self.naif_keywords.get('INS{}_BORESIGHT_SAMPLE'.format(self.ikid), None)
 
     @property
     def detector_center_line(self):
@@ -364,7 +372,8 @@ class IsisSpice():
         list :
             The center of the CCD formatted as line, sample
         """
-        return self.naif_keywords.get('INS{}_BORESIGHT_LINE'.format(self.ikid), None)
+        return get_naif_keyword(self, 'detector_center_line', 'INS{}_BORESIGHT_LINE'.format(self.ikid))
+        #return self.naif_keywords.get('INS{}_BORESIGHT_LINE'.format(self.ikid), None)
 
 
     @property
@@ -430,7 +439,8 @@ class IsisSpice():
             The coefficients of the affine transformation
             formatted as constant, x, y
         """
-        return self.naif_keywords.get('INS{}_ITRANSL'.format(self.ikid), None)
+        return get_naif_keyword(self, 'focal2pixel_lines', 'INS{}_ITRANSL'.format(self.ikid))
+        #return self.naif_keywords.get('INS{}_ITRANSL'.format(self.ikid), None)
 
     @property
     def focal2pixel_samples(self):
@@ -448,7 +458,8 @@ class IsisSpice():
             The coefficients of the affine transformation
             formatted as constant, x, y
         """
-        return self.naif_keywords.get('INS{}_ITRANSS'.format(self.ikid), None)
+        return get_naif_keyword(self, 'focal2pixel_samples', 'INS{}_ITRANSS'.format(self.ikid))
+        #return self.naif_keywords.get('INS{}_ITRANSS'.format(self.ikid), None)
 
     @property
     def pixel2focal_x(self):
@@ -461,7 +472,8 @@ class IsisSpice():
         detector to focal plane x
         """
 
-        return self.naif_keywords.get('INS{}_TRANSX'.format(self.ikid), None)
+        return get_naif_keyword(self, 'pixel2focal_x', 'INS{}_TRANSX'.format(self.ikid))
+        #return self.naif_keywords.get('INS{}_TRANSX'.format(self.ikid), None)
 
     @property
     def pixel2focal_y(self):
@@ -474,7 +486,8 @@ class IsisSpice():
         detector to focal plane y
         """
 
-        return self.naif_keywords.get('INS{}_TRANSY'.format(self.ikid), None)
+        return get_naif_keyword(self, 'pixel2focal_y', 'INS{}_TRANSY'.format(self.ikid))
+        #return self.naif_keywords.get('INS{}_TRANSY'.format(self.ikid), None)
 
     @property
     def focal_length(self):
@@ -490,7 +503,9 @@ class IsisSpice():
         float :
             The focal length in millimeters
         """
-        return self.naif_keywords.get('INS{}_FOCAL_LENGTH'.format(self.ikid), None)
+
+        return get_naif_keyword(self, 'focal_length', 'INS{}_FOCAL_LENGTH'.format(self.ikid))
+        #return self.naif_keywords.get('INS{}_FOCAL_LENGTH'.format(self.ikid), None)
 
     @property
     def target_body_radii(self):
